d27b0e2d9f951751a7854d2a373098932b1b3b4d
shivprogrammer <shiv.unadkat@gmail.com>
2017-05-30 13:17:00 -0700

    tests added for remove the minimum

--- codewars/to-do/remove-minimum.js
+++ codewars/to-do/remove-minimum.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const expect = require('chai').expect;
 /*
 Remove the Minimum
 
@@ -23,3 +24,36 @@ removeSmallest([1,2,3,4,5]) = [2,3,4,5]
 removeSmallest([5,3,2,1,4]) = [5,3,2,4]
 removeSmallest([2,2,1,2,1]) = [2,2,2,1]
 */
+
+function removeSmallest(numbers) {
+}
+
+describe('Remove the Minimum | 7kyu', function() {
+  describe('base case empty list input: []', function() {
+    it('should return empty list: []', done => {
+      expect(removeSmallest([])).to.deep.equal([]);
+      done();
+    })
+  })
+
+  describe('Input has min at 0 index: [1, 2, 3, 4, 5]', function() {
+    it('should return input with 1 removed: [2, 3, 4, 5]', done => {
+      expect(removeSmallest([1, 2, 3, 4, 5])).to.deep.equal([2, 3, 4, 5]);
+      done();
+    })
+  })
+
+  describe('Input is 1 at 3rd index: [5, 3, 2, 1, 4]', function() {
+    it('should return input with 1 removed: [5, 3, 2, 4]', done => {
+      expect(removeSmallest([5, 3, 2, 1, 4]))).to.deep.equal([5, 3, 2, 4]);
+      done();
+    })
+  })
+
+  describe('Input has 2 mins: [2, 2, 1, 2, 1]', function() {
+    it('should remove min at preceding index: [2, 2, 2, 1]', done => {
+      expect(removeSmallest([2, 2, 1, 2, 1]))).to.deep.equal([2, 2, 2, 1]);
+      done();
+    })
+  })
+})
