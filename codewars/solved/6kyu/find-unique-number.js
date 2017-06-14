2b636cc06240b884b459bf0eafb29061222d3355
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-14 15:47:50 -0700

    Find Unique Number | Code Wars | All Tests Passing | Solved in 22 Min

---
+++ codewars/solved/6kyu/find-unique-number.js
@@ -0,0 +1,60 @@
+// START 3:09 PM
+// FUCKING SOLVED WITH ALL TESTS PASSING BY 3:31 PM SONNNNNN => 22 MINUTES TOTAL
+
+'use strict';
+
+const expect = require('chai').expect;
+
+/*
+Find the Unique Number
+
+6kyu
+
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains more than 3 numbers.
+*/
+
+function findUniq(arr) {
+  var stack1 = [];
+  var stack2 = [];
+
+  while (stack2.length < 2 && stack1.length < 2) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[0]) {
+        stack1.push(arr[i]);
+      }
+      else {
+        stack2.push(arr[i]);
+      }
+    }
+  }
+
+  if (stack1.length > 1) {
+    return stack2.shift();
+  }
+  else {
+    return stack1.shift();
+  }
+}
+
+// Time Complexity: O(N) // Worst case if the unique number is literally the last one. Otherwise this is SO fucking fast
+// Space Complexity: O(N) [stack1] + O(N) [stack2]
+
+describe('Find the Unique Number | Code Wars | 6kyu', function() {
+  describe('input [0, 1, 0]', function() {
+    it('should return 1', done => {
+      expect(findUniq([0, 1, 0])).to.equal(1);
+      done();
+    })
+  })
+
+  describe('input [ 1, 1, 1, 2, 1, 1 ]', function() {
+    it('should return 2', done => {
+      expect(findUniq([ 1, 1, 1, 2, 1, 1 ])).to.equal(2);
+      done();
+    })
+  })
+})
