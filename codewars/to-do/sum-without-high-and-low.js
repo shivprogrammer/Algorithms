a886ee72f1698aedcb1e0178db51b24a42b86e6d
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-15 18:22:40 -0700

    All test passed! Sum without highest and lowest numbers problem solved

--- codewars/to-do/sum-without-high-and-low.js
+++ codewars/to-do/sum-without-high-and-low.js
@@ -18,10 +18,45 @@ If array is empty, null or None, or if only 1 Element exists, return 0.
 */
 
 function sumArray(array) {
+  if (!array || array.length < 2) {
+    return 0;
+  }
 
+  var sum = 0;
+  var high = array[0];
+  var low = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    if (array[i] > high) {
+      high = array[i];
+    }
+    if (array[i] < low) {
+      low = array[i];
+    }
+  }
+
+  return sum - high - low;
 }
 
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
 describe('Sum Without High and Low | Code Wars | 8kyu', function() {
+  describe('with empty array as input', function() {
+    it('should return 0', done => {
+      expect(sumArray([])).to.equal(0);
+      done();
+    })
+  })
+
+  describe('with array less then length 3', function() {
+    it('should return 0', done => {
+      expect(sumArray([2, 10])).to.equal(0);
+      done();
+    })
+  })
+
   describe('with input [ 6, 2, 1, 8, 10 ]', function() {
     it('should return 16', done => {
       expect(sumArray([ 6, 2, 1, 8, 10 ])).to.equal(16);
