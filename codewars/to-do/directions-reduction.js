b115ca4fed24a175b7e4892d73dd61a9b675c1d6
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-16 03:23:16 -0700

    directions reduction scaffold set up

--- codewars/to-do/directions-reduction.js
+++ codewars/to-do/directions-reduction.js
@@ -1,24 +1,15 @@
 'use strict';
 
-/*
-Once upon a time, on a way through the old wild west,…
+const expect = require('chai').expect;
 
-… a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too. Going to one direction and coming back the opposite direction is a needless effort. Since this is the wild west, with dreadfull weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+/*
+Once upon a time, on a way through the old wild west,… … a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too. Going to one direction and coming back the opposite direction is a needless effort. Since this is the wild west, with dreadfull weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
 
 How I crossed the desert the smart way.
 
-The directions given to the man are, for example, the following:
+The directions given to the man are, for example, the following: ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
 
-["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
-or
-
-{ "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
-or (haskell)
-
-[North, South, South, East, West, North, West]
-You can immediatly see that going "NORTH" and then "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
-
-["WEST"]
+You can immediatly see that going "NORTH" and then "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply: ["WEST"]
 
 Other examples:
 
@@ -28,19 +19,39 @@ The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, 
 
 In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
 
-Task
-
+Task:
 Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
 
-The Haskell version takes a list of directions with data Direction = North | East | West | South. The Clojure version returns nil when the path is reduced to nothing. The Rust version takes a slice of enum Direction {NORTH, SOUTH, EAST, WEST}.
-
-Examples
-
+Examples:
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]) => ["WEST"]
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]) => []
-See more examples in "Example Tests"
 
 Note
-
 All paths can't be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"] is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST" are not directly opposite of each other and can't become such. Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
 */
+
+function dirReduc(arr){
+}
+
+describe('Directions Reduction | Code Wars | 5kyu', function() {
+  describe('Input: ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]', function() {
+    it('should return ["WEST"]', done => {
+      expect(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])).to.equal(["WEST"]);
+      done();
+    })
+  })
+
+  describe('Input: ["NORTH", "WEST", "SOUTH", "EAST"]', function() {
+    it('should return ["NORTH", "WEST", "SOUTH", "EAST"]', done => {
+      expect(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])).to.equal(["NORTH", "WEST", "SOUTH", "EAST"]);
+      done();
+    })
+  })
+
+  describe('Input: ["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]', function() {
+    it('should return []', done => {
+      expect(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])).to.equal([]);
+      done();
+    })
+  })
+})
