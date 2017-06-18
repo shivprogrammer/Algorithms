6f76c373c70a8a74a4584f6cd0bf732f2479e32f
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-17 20:24:54 -0700

    scaffold set up for reverse number

--- to-do/reverse-number.js
+++ to-do/reverse-number.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const expect = require('chai').expect;
+
+/*
+Convert number to reversed array of digits
+
+8kyu
+
+Given a random number:
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+*/
+
+function digitize(n) {
+}
+
+Test.assertSimilar(digitize(35231),[1,3,2,5,3]);
