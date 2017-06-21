0ac3060b598b6edf08210a85393d889714664394
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-21 09:39:15 -0700

    tests added for needle in a haystack

--- codewars/to-do/needle-in-haystack.js
+++ codewars/to-do/needle-in-haystack.js
@@ -28,10 +28,25 @@ should return
 function findNeedle(haystack) {
 }
 
-var haystack_1 = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false];
-var haystack_2 = ['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'];
-var haystack_3 = [1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54];
+describe('Needle in a Haystack | Code Wars | 8kyu', function() {
+  describe('Input: ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]', function() {
+    it('should return "found the needle at position 3"', done => {
+      expect(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false])).to.equal('found the needle at position 3');
+      done();
+    })
+  })
 
-Test.assertEquals(findNeedle(haystack_1), 'found the needle at position 3')
-Test.assertEquals(findNeedle(haystack_2), 'found the needle at position 5')
-Test.assertEquals(findNeedle(haystack_3), 'found the needle at position 30')
+  describe('Input: ['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']', function() {
+    it('should return "found the needle at position 5"', done => {
+      expect(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'])).to.equal('found the needle at position 5');
+      done();
+    })
+  })
+
+  describe('Input: [1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54]', function() {
+    it('should return "found the needle at position 30"', done => {
+      expect(findNeedle([1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54])).to.equal('found the needle at position 30');
+      done();
+    })
+  })
+})
