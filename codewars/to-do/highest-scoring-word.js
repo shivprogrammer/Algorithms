412ec99039b5aef49941cb53f0de11511972555d
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-22 02:14:09 -0700

    local variables, for loops, and alphabet map created

--- codewars/to-do/highest-scoring-word.js
+++ codewars/to-do/highest-scoring-word.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Highest Scoring Word
 
@@ -12,7 +14,43 @@ If two words score the same, return the word that appears earliest in the origin
 All letters will be lower case and all inputs will be valid.
 */
 
-function high(x){
+function high(x) {
+  var topScore = 0;
+  var tempScore = 0;
+
+  var alphabetMap = new Map();
+  alphabetMap.set('a': 1);
+  alphabetMap.set('b': 2);
+  alphabetMap.set('c', 3);
+  alphabetMap.set('d': 4);
+  alphabetMap.set('e': 5);
+  alphabetMap.set('f': 6);
+  alphabetMap.set('g': 7);
+  alphabetMap.set('h': 8);
+  alphabetMap.set('i': 9);
+  alphabetMap.set('j': 10);
+  alphabetMap.set('k': 11);
+  alphabetMap.set('l': 12);
+  alphabetMap.set('m': 13);
+  alphabetMap.set('n': 14);
+  alphabetMap.set('o': 15);
+  alphabetMap.set('p': 16);
+  alphabetMap.set('q': 17);
+  alphabetMap.set('r': 18);
+  alphabetMap.set('s': 19);
+  alphabetMap.set('t': 20);
+  alphabetMap.set('u': 21);
+  alphabetMap.set('v': 22);
+  alphabetMap.set('w': 23);
+  alphabetMap.set('x': 24);
+  alphabetMap.set('y': 25);
+  alphabetMap.set('z': 26);
+
+  for (let i = 0; i < x.length; i++) {
+    for (let j = 0; j < x[i].length; j++) {
+
+    }
+  }
 }
 
 describe('Highest Scoring Word | Code Wars | 6kyu', function() {
