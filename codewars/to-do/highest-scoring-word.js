4b0f53d3131baaf794f5638ba09401f1326cfd6a
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-22 02:44:56 -0700

    2 tests passing, one failing for highest scoring word

--- codewars/to-do/highest-scoring-word.js
+++ codewars/to-do/highest-scoring-word.js
@@ -15,42 +15,57 @@ All letters will be lower case and all inputs will be valid.
 */
 
 function high(x) {
+  var topWord = '';
   var topScore = 0;
   var tempScore = 0;
 
   var alphabetMap = new Map();
-  alphabetMap.set('a': 1);
-  alphabetMap.set('b': 2);
+  alphabetMap.set('a', 1);
+  alphabetMap.set('b', 2);
   alphabetMap.set('c', 3);
-  alphabetMap.set('d': 4);
-  alphabetMap.set('e': 5);
-  alphabetMap.set('f': 6);
-  alphabetMap.set('g': 7);
-  alphabetMap.set('h': 8);
-  alphabetMap.set('i': 9);
-  alphabetMap.set('j': 10);
-  alphabetMap.set('k': 11);
-  alphabetMap.set('l': 12);
-  alphabetMap.set('m': 13);
-  alphabetMap.set('n': 14);
-  alphabetMap.set('o': 15);
-  alphabetMap.set('p': 16);
-  alphabetMap.set('q': 17);
-  alphabetMap.set('r': 18);
-  alphabetMap.set('s': 19);
-  alphabetMap.set('t': 20);
-  alphabetMap.set('u': 21);
-  alphabetMap.set('v': 22);
-  alphabetMap.set('w': 23);
-  alphabetMap.set('x': 24);
-  alphabetMap.set('y': 25);
-  alphabetMap.set('z': 26);
+  alphabetMap.set('d', 4);
+  alphabetMap.set('e', 5);
+  alphabetMap.set('f', 6);
+  alphabetMap.set('g', 7);
+  alphabetMap.set('h', 8);
+  alphabetMap.set('i', 9);
+  alphabetMap.set('j', 10);
+  alphabetMap.set('k', 11);
+  alphabetMap.set('l', 12);
+  alphabetMap.set('m', 13);
+  alphabetMap.set('n', 14);
+  alphabetMap.set('o', 15);
+  alphabetMap.set('p', 16);
+  alphabetMap.set('q', 17);
+  alphabetMap.set('r', 18);
+  alphabetMap.set('s', 19);
+  alphabetMap.set('t', 20);
+  alphabetMap.set('u', 21);
+  alphabetMap.set('v', 22);
+  alphabetMap.set('w', 23);
+  alphabetMap.set('x', 24);
+  alphabetMap.set('y', 25);
+  alphabetMap.set('z', 26);
 
   for (let i = 0; i < x.length; i++) {
-    for (let j = 0; j < x[i].length; j++) {
+    if (x.charAt(i) === ' ') {
+      if (tempScore > topScore) {
+        topScore = tempScore;
+        tempScore = 0;
+      }
+      else {
+        tempScore = 0;
+        topWord = '';
+      }
+    }
 
+    else {
+      tempScore += alphabetMap.get(x.charAt(i));
+      topWord += x.charAt(i);
     }
   }
+
+  return topWord;
 }
 
 describe('Highest Scoring Word | Code Wars | 6kyu', function() {
