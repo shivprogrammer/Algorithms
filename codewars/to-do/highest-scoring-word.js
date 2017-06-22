635ee2f730411823f0d6a13cf803250afe44bca6
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-22 02:00:15 -0700

    tests added for highest scoring word

--- codewars/to-do/highest-scoring-word.js
+++ codewars/to-do/highest-scoring-word.js
@@ -6,20 +6,34 @@ Highest Scoring Word
 6kyu
 
 Given a string of words (x), you need to find the highest scoring word.
-
 Each letter of a word scores points according to it's position in the alphabet. a=1, z=26 and everything inbetween.
-
 You need to return the highest scoring word as a string.
-
 If two words score the same, return the word that appears earliest in the original string.
-
 All letters will be lower case and all inputs will be valid.
 */
 
 function high(x){
-
 }
 
-Test.assertEquals(high('man I need a taxi up to ubud'), 'taxi');
-Test.assertEquals(high('what time are we climbing up the volcano'), 'volcano');
-Test.assertEquals(high('take me to semynak'), 'semynak');
+describe('Highest Scoring Word | Code Wars | 6kyu', function() {
+  describe('Input: "man I need a taxi up to ubud"', function() {
+    it('should return "taxi"', done => {
+      expect(high('man I need a taxi up to ubud')).to.equal('taxi');
+      done();
+    })
+  })
+
+  describe('Input: "what time are we climbing up the volcano"', function() {
+    it('should return "volcano"', done => {
+      expect(high('what time are we climbing up the volcano')).to.equal('volcano');
+      done();
+    })
+  })
+
+  describe('Input: "take me to semynak"', function() {
+    it('should return "semynak"', done => {
+      expect(high('take me to semynak')).to.equal('semynak');
+      done();
+    })
+  })
+})
