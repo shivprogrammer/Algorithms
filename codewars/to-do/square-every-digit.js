c48d7a0929b3fa0c67ffd7664cbe4b9972088cd2
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-22 20:52:22 -0700

    all tests passing on code wars for square every digit yeeeeeeeee

--- codewars/to-do/square-every-digit.js
+++ codewars/to-do/square-every-digit.js
@@ -14,7 +14,14 @@ For example, if we run 9119 through the function, 811181 will come out.
 Note: The function accepts an integer and returns an integer
 */
 
-function squareDigits(num){
+function squareDigits(num) {
+  var output = '';
+
+  for (let i = 0; i < num.toString(); i++) {
+    output += Math.pow(parseInt(num.toString().charAt(i)), 2);
+  }
+
+  return parseInt(output);
 }
 
 describe('Square Every Digit | Code Wars | 7kyu', function() {
