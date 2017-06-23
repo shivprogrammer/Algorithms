e2c6e3b8683d74605b5692ad6791fb4aba34eb75
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-22 20:49:13 -0700

    square every digit test added

--- codewars/to-do/square-every-digit.js
+++ codewars/to-do/square-every-digit.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+Square Every Digit
+
+7kyu
+
 Welcome. In this kata, you are asked to square every digit of a number.
 
 For example, if we run 9119 through the function, 811181 will come out.
@@ -11,4 +17,11 @@ Note: The function accepts an integer and returns an integer
 function squareDigits(num){
 }
 
-Test.assertEquals(squareDigits(9119), 811181);
+describe('Square Every Digit | Code Wars | 7kyu', function() {
+  describe('Input: 9119', function() {
+    it('return 811181', done => {
+      expect(squareDigits(9119)).to.equal(811181);
+      done();
+    })
+  })
+})
