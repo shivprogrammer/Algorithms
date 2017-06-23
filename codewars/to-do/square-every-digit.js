eb0d88a1ee23731059f26e98a7e9eb8491377251
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-22 20:47:12 -0700

    square every digit set up complete

--- codewars/to-do/square-every-digit.js
+++ codewars/to-do/square-every-digit.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/*
+Welcome. In this kata, you are asked to square every digit of a number.
+
+For example, if we run 9119 through the function, 811181 will come out.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+function squareDigits(num){
+}
+
+Test.assertEquals(squareDigits(9119), 811181);
