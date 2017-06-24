6abea5186086f821b2f4e8b7946b062d1605a2d0
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-24 00:32:10 -0700

    tests added for merged string checker

--- codewars/to-do/merged-string-checker.js
+++ codewars/to-do/merged-string-checker.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+Merged String Checker
+
+5kyu
+
 At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
 
 The restriction is that the characters in part1 and part2 are in the same order as in s.
@@ -20,6 +26,25 @@ function isMerge(s, part1, part2) {
   return false;
 }
 
-Test.expect(isMerge('codewars', 'code', 'wars'));
-Test.expect(isMerge('codewars', 'cdw', 'oears'));
-Test.expect(!isMerge('codewars', 'cod', 'wars'));
+describe('Merged String Checker | Code Wars | 5kyu', function() {
+  describe('with input s = codewars, part1 = code, part2 = wars', function() {
+    it('should return true', done => {
+      expect(isMerge('codewars', 'code', 'wars')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('with input s = codewars, part1 = cdw, part2 = oears', function() {
+    it('should return true', done => {
+      expect(isMerge('codewars', 'cdw', 'oears')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('with input s = codewars, part1 = cod, part2 = wars', function() {
+    it('should return false', done => {
+      expect(isMerge('codewars', 'cod', 'wars')).to.equal(true);
+      done();
+    })
+  })
+})
