26a20d75c8b3330e66d7cb954dce974805127725
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-23 20:12:52 -0700

    shortest word set up complete

--- codewars/to-do/shortest-word.js
+++ codewars/to-do/shortest-word.js
@@ -1,1 +1,17 @@
 'use strict';
+
+/*
+Shortest Word
+
+7kyu
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s){
+}
+
+Test.assertEquals(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+Test.assertEquals(findShort("turns out random test cases are easier than writing out basic ones"), 3);
