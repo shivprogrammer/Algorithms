1ac6ce8c5ee91dac2e9fc2a468002d29099d1b70
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-24 02:27:04 -0700

    sum strings scaffold and test

--- codewars/to-do/sum-strings.js
+++ codewars/to-do/sum-strings.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const expect = require('chai').expect;
+
+/*
+Sum Strings as Numbers
+
+4kyu
+
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+*/
+
+function sumStrings(a,b) {
+}
+
+describe('Sum Strings as Numbers | Code Wars | 4kyu', function() {
+  describe('Input: "123", "456"', function() {
+    it('Expect: "579"', done => {
+      expect(sumStrings('123','456')).to.equal('579');
+      done();
+    })
+  })
+})
