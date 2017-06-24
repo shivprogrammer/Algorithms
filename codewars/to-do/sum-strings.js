d3cbae787e313d867f4f859fdb670aa91e6c74c8
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-24 02:50:07 -0700

    progress on sum strings

--- codewars/to-do/sum-strings.js
+++ codewars/to-do/sum-strings.js
@@ -16,12 +16,68 @@ A string representation of an integer will contain no characters besides the ten
 */
 
 function sumStrings(a,b) {
+  if (!a) {
+    return b;
+  }
+
+  if (!b) {
+    return a;
+  }
+
+  var output = '';
+  var loopLength = 0;
+
+  if (b.length > a.length) {
+    loopLength = b.length;
+  }
+  if (b.length < a.length) {
+    loopLength = a.length;
+  }
+  if (a.length === b.length) {
+    if (parseInt(a.charAt(0)) + parseInt(b.charAt(0)) >= 10) {
+      loopLength = a.length + 1;
+    }
+    else {
+      loopLength = a.length;
+    }
+  }
+
+  while (loopLength) {
+    if (a.length && b.length) {
+      output += parseInt(a.charAt(a.length - 1)) + parseInt(b.charAt(b.length - 1));
+      a = Math.floor(parseInt(a) / 10);
+      b = Math.floor(parseInt(b) / 10);
+      loopLength--;
+    }
+    if (a.length) {
+      return a + output;
+    }
+    if (b.length) {
+      return b + output;
+    }
+  }
+
+  return output;
 }
 
 describe('Sum Strings as Numbers | Code Wars | 4kyu', function() {
+  describe('one input value is empty string', function() {
+    it('Expect: "5"', done => {
+      expect(sumStrings('','5')).to.equal('5');
+      done();
+    })
+  })
+
   describe('Input: "123", "456"', function() {
     it('Expect: "579"', done => {
       expect(sumStrings('123','456')).to.equal('579');
+      done();
+    })
+  })
+
+  describe('Input: "712569312664357328695151392", "8100824045303269669937"', function() {
+    it('Expect: "712577413488402631964821329"', done => {
+      expect(sumStrings('712569312664357328695151392','8100824045303269669937')).to.equal('712577413488402631964821329');
       done();
     })
   })
