ef932be802334c2cd504f18c8a38481e8462ae0e
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-23 23:02:58 -0700

    human readble time scaffold set up

--- codewars/to-do/human-readable-time.js
+++ codewars/to-do/human-readable-time.js
@@ -1,1 +1,23 @@
 'use strict';
+
+/*
+Human Readable Time
+
+5kyu
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+*/
+
+function humanReadable(seconds) {
+}
+
+Test.assertEquals(humanReadable(0), '00:00:00', 'humanReadable(0)');
+Test.assertEquals(humanReadable(5), '00:00:05', 'humanReadable(5)');
+Test.assertEquals(humanReadable(60), '00:01:00', 'humanReadable(60)');
+Test.assertEquals(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
+Test.assertEquals(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
