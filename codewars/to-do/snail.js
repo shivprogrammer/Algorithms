1ef2e6585dab3093e2223d57ec967d0795f96a2e
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-23 20:08:02 -0700

    started shortest word | code wars | 7kyu

--- codewars/to-do/snail.js
+++ codewars/to-do/snail.js
@@ -20,5 +20,21 @@ snail(array) #=> [1,2,3,4,5,6,7,8,9]
 */
 
 function snail(array) {
-  
+
 }
+
+describe('Snail | Code Wars | 4kyu', function() {
+  describe('Input: [[1,2,3], [4,5,6], [7,8,9]]', function() {
+    it('should return [1,2,3,6,9,8,7,4,5]', done => {
+      expect(snail([[1,2,3], [4,5,6], [7,8,9]])).to.deep.equal([1,2,3,6,9,8,7,4,5]);
+      done();
+    })
+  })
+
+  describe('Input: [[1,2,3], [8,9,4], [7,6,5]]', function() {
+    it('should return [1,2,3,4,5,6,7,8,9]', done => {
+      expect(snail([[1,2,3], [8,9,4], [7,6,5]])).to.deep.equal([1,2,3,4,5,6,7,8,9]);
+      done();
+    })
+  })
+})
