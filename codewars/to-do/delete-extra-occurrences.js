36af833eebb843db8a2fe09302f39243afcc34ff
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-25 16:13:54 -0700

    function and assert tests added for delete extra occurrences

--- codewars/to-do/delete-extra-occurrences.js
+++ codewars/to-do/delete-extra-occurrences.js
@@ -11,7 +11,13 @@ Given a list lst and a number N, create a new list that contains each number of 
 
 Example
 
-  deleteNth ([1,1,1,1],2) // return [1,1]
+deleteNth ([1,1,1,1],2) // return [1,1]
 
-  deleteNth ([20,37,20,21],1) // return [20,37,21]
+deleteNth ([20,37,20,21],1) // return [20,37,21]
 */
+
+function deleteNth(arr, x) {
+}
+
+Test.assertSimilar(deleteNth([20,37,20,21], 1), [20,37,21])
+Test.assertSimilar(deleteNth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
