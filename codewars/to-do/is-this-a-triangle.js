66a048211cb1ccb0a973fd822cbd6b7f76e9241e
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-26 11:27:54 -0700

    all local tests passing for is this a triangle, but code wars bugs

--- codewars/to-do/is-this-a-triangle.js
+++ codewars/to-do/is-this-a-triangle.js
@@ -12,7 +12,35 @@ Implement a method that accepts 3 integer values a, b, c. The method should retu
 (In this case, all triangles must have surface greater than 0 to be accepted).
 */
 
-function isTriangle(a,b,c)
+function isTriangle(a,b,c) {
+  var high = Math.max(a,b,c);
+
+  if (high === a) {
+    if (Math.pow(b, 2) + Math.pow(c, 2) >= Math.pow(high, 2)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  if (high === b) {
+    if (Math.pow(a, 2) + Math.pow(c, 2) >= Math.pow(high, 2)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  if (high === c) {
+    if (Math.pow(a, 2) + Math.pow(b, 2) >= Math.pow(high, 2)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
 
 describe('Is This a Triangle? | 7kyu', function() {
