da87623d6f29829b785e44eebdd5f1f968965073
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-26 13:27:42 -0700

    progress on the simple pig latin question, just working on last word now

--- codewars/to-do/simple-pig-latin.js
+++ codewars/to-do/simple-pig-latin.js
@@ -3,11 +3,35 @@
 const expect = require('chai').expect
 
 /*
+Simple Pig Latin
+
+5kyu
+
 Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
+
 pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 */
 
 function pigIt(str) {
+  var output = '';
+  var tempLetter = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i === 0 || tempLetter === '') {
+      tempLetter = str.charAt(i);
+    }
+
+    if (str.charAt(i) === ' ') {
+      output += tempLetter + 'ay';
+      tempLetter = '';
+    }
+
+    if (i != 0 && !(str.charAt(i - 1) === ' ')) {
+      output += str.charAt(i);
+    }
+  }
+
+  return output;
 }
 
 describe('Pig Latin Question Code Wars', function() {
