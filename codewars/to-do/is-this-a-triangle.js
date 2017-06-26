0d9065a106329497daaca7d8363dad9c849efa9c
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-26 06:57:15 -0700

    is this a triangle set up complete

--- codewars/to-do/is-this-a-triangle.js
+++ codewars/to-do/is-this-a-triangle.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const expect = require('chai').expect;
+
+/*
+Is This a Triangle?
+
+7kyu
+
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+*/
+
+function isTriangle(a,b,c)
+}
+
+describe('Is This a Triangle? | 7kyu', function() {
+  describe('Input: 1, 2, 2', function() {
+    it('Expected: true', done => {
+      expect(isTriangle(1,2,2)).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: 7, 2, 2', function() {
+    it('Expected: false', done => {
+      expect(isTriangle(7,2,2)).to.equal(false);
+      done();
+    })
+  })
+})
