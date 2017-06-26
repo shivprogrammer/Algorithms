b3cbe37f66dec68f1d9022f23fd6904132a37579
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-26 13:11:42 -0700

    simple pig latin | 5kyu scaffold and tests written

--- codewars/to-do/greed-is-good.js
+++ codewars/to-do/greed-is-good.js
@@ -1,6 +1,8 @@
+// ALL TESTS PASSING, BUT THERE IS A TYPEERROR MAP IS NOT A FUNCTION BS ON CODE WARS
 'use strict';
 
 const expect = require('chai').expect;
+
 /*
 Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
 
