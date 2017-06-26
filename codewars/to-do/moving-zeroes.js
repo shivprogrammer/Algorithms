58f17d3091e99a13f824cdfd1d8a41c16896905f
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-26 00:32:29 -0700

    moving zeroes all tests passed. I'm getting fast

--- codewars/to-do/moving-zeroes.js
+++ codewars/to-do/moving-zeroes.js
@@ -12,7 +12,20 @@ Write an algorithm that takes an array and moves all of the zeros to the end, pr
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
-function moveZeroes(arr) {
+function moveZeros(arr) {
+  var nonZeros = [];
+  var zeros = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeros.push(arr[i]);
+    }
+    else {
+      nonZeros.push(arr[i]);
+    }
+  }
+
+  return nonZeros.concat(zeros);
 }
 
 describe('Moving Zeroes to the End | 5kyu', function() {
