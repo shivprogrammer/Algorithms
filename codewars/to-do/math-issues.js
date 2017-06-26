dab1d4d262d4102477f2509fd12bd16068be4a60
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-26 13:58:10 -0700

    all tests passed on math issues FIRST TRY :)

--- codewars/to-do/math-issues.js
+++ codewars/to-do/math-issues.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
+
 /*
 Math Issues
 
@@ -16,12 +17,15 @@ Math.floor()
 */
 
 Math.round = function(number) {
+  return number % 1 >= 0.5 ? number + (1 -(number % 1)) : number - number % 1;
 }
 
 Math.ceil = function(number) {
+  return number % 1 > 0 ? number + (1 -(number % 1)) : number;
 }
 
 Math.floor = function(number) {
+  return number % 1 > 0 ? number - number % 1 : number;
 }
 
 describe('Math Issues | Code Wars | 5kyu', function() {
