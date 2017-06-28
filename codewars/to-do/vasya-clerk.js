fabb1c835ba9582a11b008cee8520301fc57ca68
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-28 13:40:47 -0700

    tests added for vasya clerk

--- codewars/to-do/vasya-clerk.js
+++ codewars/to-do/vasya-clerk.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+Vasya - Clerk
+
+6kyu
+
 The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. A "Avengers" ticket costs 25 dollars.
 
 Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
@@ -16,3 +22,22 @@ Return YES, if Vasya can sell a ticket to each person and give the change. Other
 tickets([25, 25, 50]) // => YES
 tickets([25, 100]) // => NO. Vasya will not have enough money to give change to 100 dollars
 */
+
+function tickets(peopleInLine) {
+}
+
+describe('Vasya - Clerk | 6kyu', function() {
+  describe('Input: [25, 25, 50, 50]', function() {
+    it('should return "YES"', done => {
+      expect(tickets([25, 25, 50, 50])).to.equal('YES');
+      done();
+    })
+  })
+
+  describe('Input: [25, 100]', function() {
+    it('should return "NO"', done => {
+      expect(tickets([25, 100])).to.equal('NO');
+      done();
+    })
+  })
+})
