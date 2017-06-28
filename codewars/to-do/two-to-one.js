0ff2bfa10b520cef152af3ca8de1856c4b156b44
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-27 20:18:26 -0700

    two to one all tests passed!

--- codewars/to-do/two-to-one.js
+++ codewars/to-do/two-to-one.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Two to One
 
@@ -12,9 +14,45 @@ a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" `
 */
 
 function longest(s1, s2) {
-  // your code
+  var alphabetMap = new Map();
+  var output = '';
+
+  for (let i = 0; i < s1.length; i++) {
+    if (!alphabetMap.has(s1.charAt(i))) {
+      output += s1.charAt(i);
+    }
+    alphabetMap.set(s1.charAt(i), alphabetMap.get(s1.charAt(i)) + 1);
+  }
+
+  for (let j = 0; j < s2.length; j++) {
+    if (!alphabetMap.has(s2.charAt(j))) {
+      output += s2.charAt(j)
+    }
+    alphabetMap.set(s2.charAt(j), alphabetMap.get(s2.charAt(j)) + 1);
+  }
+
+  return output.split('').sort().join('');
 }
 
-Test.assertEquals(longest("aretheyhere", "yestheyarehere"), "aehrsty")
-Test.assertEquals(longest("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu")
-Test.assertEquals(longest("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
+describe('Two to One | 7kyu', function() {
+  describe('with inputs "aretheyhere", "yestheyarehere"', function() {
+    it('should return "aehrsty"', done => {
+      expect(longest('aretheyhere', 'yestheyarehere')).to.equal('aehrsty');
+      done();
+    })
+  })
+
+  describe('with inputs "loopingisfunbutdangerous", "lessdangerousthancoding"', function() {
+    it('should return "abcdefghilnoprstu"', done => {
+      expect(longest('loopingisfunbutdangerous', 'lessdangerousthancoding')).to.equal('abcdefghilnoprstu');
+      done();
+    })
+  })
+
+  describe('with inputs "inmanylanguages", "theresapairoffunctions"', function() {
+    it('should return "acefghilmnoprstuy"', done => {
+      expect(longest('inmanylanguages', 'theresapairoffunctions')).to.equal('acefghilmnoprstuy');
+      done();
+    })
+  })
+})
