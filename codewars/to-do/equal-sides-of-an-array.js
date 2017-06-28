46d40328e7218b00668346508b091d740d3484b7
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-27 20:41:48 -0700

    tests written for equal sides of an array problem

--- codewars/to-do/equal-sides-of-an-array.js
+++ codewars/to-do/equal-sides-of-an-array.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+Equal Sides of an array
+
+6kyu
+
 You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
 
 For example:
@@ -31,3 +37,36 @@ Note:
 If you are given an array with multiple answers, return the lowest correct index.
 An empty array should be treated like a 0 in this problem.
 */
+
+function findEvenIndex(arr) {
+}
+
+describe('Equal Sides of An Array | 6kyu', function() {
+  describe('Input: [1,2,3,4,3,2,1]', function() {
+    it('should return 3', done => {
+      expect(findEvenIndex([1,2,3,4,3,2,1])).to.equal(3);
+      done();
+    })
+  })
+
+  describe('Input: [1,100,50,-51,1,1]', function() {
+    it('should return 1', done => {
+      expect(findEvenIndex([1,100,50,-51,1,1])).to.equal(1);
+      done();
+    })
+  })
+
+  describe('Input: [1,2,3,4,5,6]', function() {
+    it('should return -1', done => {
+      expect(findEvenIndex([1,2,3,4,5,6])).to.equal(-1);
+      done();
+    })
+  })
+
+  describe('Input: [20,10,30,10,10,15,35]', function() {
+    it('should return 3', done => {
+      expect(findEvenIndex([20,10,30,10,10,15,35])).to.equal(3);
+      done();
+    })
+  })
+})
