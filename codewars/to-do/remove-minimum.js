aaaa4f5e47173bf5f2b8c428fa5f42a6d09ae859
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-29 13:23:11 -0700

    all tests passing for remove minimum!

--- codewars/to-do/remove-minimum.js
+++ codewars/to-do/remove-minimum.js
@@ -1,31 +1,45 @@
 'use strict';
 
 const expect = require('chai').expect;
+
 /*
 Remove the Minimum
 
 7kyu
 
 The museum of incredible dull things
-
 The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
-
 However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
 
 Task
-
 Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
-
 Don't change the order of the elements that are left.
 
 Examples
-
 removeSmallest([1,2,3,4,5]) = [2,3,4,5]
 removeSmallest([5,3,2,1,4]) = [5,3,2,4]
 removeSmallest([2,2,1,2,1]) = [2,2,2,1]
 */
 
 function removeSmallest(numbers) {
+  var minVal = Infinity;
+  var minIndex = 0;
+  var output = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < minVal) {
+      minVal = numbers[i];
+      minIndex = i;
+    }
+  }
+
+  for (let j = 0; j < numbers.length; j++) {
+    if (j != minIndex) {
+      output.push(numbers[j]);
+    }
+  }
+
+  return output;
 }
 
 describe('Remove the Minimum | 7kyu', function() {
@@ -45,14 +59,14 @@ describe('Remove the Minimum | 7kyu', function() {
 
   describe('Input is 1 at 3rd index: [5, 3, 2, 1, 4]', function() {
     it('should return input with 1 removed: [5, 3, 2, 4]', done => {
-      expect(removeSmallest([5, 3, 2, 1, 4]))).to.deep.equal([5, 3, 2, 4]);
+      expect(removeSmallest([5, 3, 2, 1, 4])).to.deep.equal([5, 3, 2, 4]);
       done();
     })
   })
 
   describe('Input has 2 mins: [2, 2, 1, 2, 1]', function() {
     it('should remove min at preceding index: [2, 2, 2, 1]', done => {
-      expect(removeSmallest([2, 2, 1, 2, 1]))).to.deep.equal([2, 2, 2, 1]);
+      expect(removeSmallest([2, 2, 1, 2, 1])).to.deep.equal([2, 2, 2, 1]);
       done();
     })
   })
