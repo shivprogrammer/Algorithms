13f125b20ddf5d1e720ffb2cf7465e85786ba404
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-30 15:06:26 -0700

    MAD PROGRESS ON THAT TEN BOWLING SONNNN

--- codewars/to-do/ten-pin-bowling.js
+++ codewars/to-do/ten-pin-bowling.js
@@ -5,32 +5,49 @@ const expect = require('chai').expect;
 /*
 Given a string, calculate the score of a bowling game.
 
+If strike, add in scores of the following whole frame to the current one.
+
+If spare, add in the score of the first immediate following shot
+
 */
 
 function bowling(string) {
   var score = 0;
-  var frames = string.split('');
+  var frames = string.split(' ');
+  var tempScore = 0;
+  var frameTotals = [];
 
   for (let i = 0; i < frames.length; i++) {
-    if (frames[i] != ' ') {
-      score += parseInt(frames[i]);
+    for (let j = 0; j < 1; j++) {
+      if (!frames[i].charAt[2]) {
+        if (Number.isInteger(parseInt(frames[i].charAt(0))) && Number.isInteger(parseInt(frames[i].charAt(1)))) {
+          tempScore = parseInt(frames[i].charAt(0)) + parseInt(frames[i].charAt(1));
+          frameTotals[i] = tempScore;
+          tempScore = 0;
+        }
+      }
     }
+  }
+  
+  for (let x = 0; x < frameTotals.length; x++) {
+    score += frameTotals[x];
   }
 
   return score;
 }
 
+
 describe('Bowling | 4kyu', function() {
-  describe('Input: "54 45 54 45 54 45 45 54 54 621"', function() {
-    it('should return 90', done => {
-      expect(bowling("54 45 54 45 54 45 45 54 54 621")).to.equal(90);
+  describe('Input: "54 45 54 45 54 45 45 54 54 62"', function() {
+    it('should return 89', done => {
+      expect(bowling("54 45 54 45 54 45 45 54 54 62")).to.equal(89);
       done();
     })
   })
 
-  describe('Input: "54 45 54 45 54 45 45 54 54 621"', function() {
-    it('should return 90', done => {
-      expect(bowling("54 45 54 45 54 45 45 54 5/ 621")).to.equal(97);
+  describe('Input: "54 45 54 45 54 45 45 54 5/ 62"', function() {
+    it('should return 96', done => {
+      expect(bowling("54 45 54 45 54 45 45 54 5/ 62")).to.equal(96);
       done();
     })
   })
