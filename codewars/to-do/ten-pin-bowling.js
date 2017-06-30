b8e63bb3ec56f6a8402817b8691eadb502f62ea0
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-30 14:17:21 -0700

    basic instructions and tests added for ten pin bowling

--- codewars/to-do/ten-pin-bowling.js
+++ codewars/to-do/ten-pin-bowling.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const expect = require('chai').expect;
+
+/*
+Given a string, calculate the score of a bowling game.
+
+*/
+
+function bowling(string) {
+
+}
+
+describe('Bowling | 4kyu', function() {
+  describe('Input: "54 45 54 45 54 45 45 54 54 621"', function() {
+    it('should return 90', done => {
+      expect(bowling("54 45 54 45 54 45 45 54 54 621")).to.equal(90);
+      done();
+    })
+  })
+
+  describe('Input: "54 45 54 45 54 45 45 54 54 621"', function() {
+    it('should return 90', done => {
+      expect(bowling("54 45 54 45 54 45 45 54 5/ 621")).to.equal(97);
+      done();
+    })
+  })
+})
