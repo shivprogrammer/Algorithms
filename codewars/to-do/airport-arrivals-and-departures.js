d12912c36510e61807e9b2552ad0df6fe65747cd
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-30 21:40:26 -0700

    created alphabetMap for airport arrivals problem

--- codewars/to-do/airport-arrivals-and-departures.js
+++ codewars/to-do/airport-arrivals-and-departures.js
@@ -6,12 +6,53 @@ Given a string and 3 ints, return the new string that you receive for the airpor
 var tickets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 function airport(str, a, b, c) {
+  var alphabetMap = new Map();
+  alphabetMap.set('A', 0);
+  alphabetMap.set('B', 1);
+  alphabetMap.set('C', 2);
+  alphabetMap.set('D', 3);
+  alphabetMap.set('E', 4);
+  alphabetMap.set('F', 5);
+  alphabetMap.set('G', 6);
+  alphabetMap.set('H', 7);
+  alphabetMap.set('I', 8);
+  alphabetMap.set('J', 9);
+  alphabetMap.set('K', 10);
+  alphabetMap.set('L', 11);
+  alphabetMap.set('M', 12);
+  alphabetMap.set('N', 13);
+  alphabetMap.set('O', 14);
+  alphabetMap.set('P', 15);
+  alphabetMap.set('Q', 16);
+  alphabetMap.set('R', 17);
+  alphabetMap.set('S', 18);
+  alphabetMap.set('T', 19);
+  alphabetMap.set('U', 20);
+  alphabetMap.set('V', 21);
+  alphabetMap.set('W', 22);
+  alphabetMap.set('X', 23);
+  alphabetMap.set('Y', 24);
+  alphabetMap.set('Z', 25);
+  alphabetMap.set('1', 26);
+  alphabetMap.set('2', 27);
+  alphabetMap.set('3', 28);
+  alphabetMap.set('4', 29);
+  alphabetMap.set('5', 30);
+  alphabetMap.set('6', 31);
+  alphabetMap.set('7', 32);
+  alphabetMap.set('8', 33);
+  alphabetMap.set('9', 34);
+  alphabetMap.set('0', 35);
+
+  var output = '';
+
+  output += tickets.charAt(str.charAt(0))
 }
 
 describe('Airport problem', function() {
-  describe('Input: ABC, 1, 5, 10', function() {
-    it('should return BGR', done => {
-      expect(airport('ABC', 1, 5, 10)).to.equal('BGR');
+  describe('Input: AAA, 1, 1, 1', function() {
+    it('should return BCD', done => {
+      expect(airport('AAA', 1, 1, 1)).to.equal('BCD');
       done();
     })
   })
@@ -23,5 +64,3 @@ describe('Airport problem', function() {
     })
   })
 })
-
-B G R
