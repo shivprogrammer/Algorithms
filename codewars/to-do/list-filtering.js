7a17b18273ab7693ba3d9724c9d60892e5bbe766
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-01 00:58:40 -0700

    tests added for list filtering

--- codewars/to-do/list-filtering.js
+++ codewars/to-do/list-filtering.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+List Filtering
+
+7kyu
+
 In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
 Example
@@ -9,3 +15,29 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
+
+function filter_list(l) {
+}
+
+describe('List Filtering | 7kyu', function() {
+  describe('Input: [1,2,'a','b']', function() {
+    it('Expected Output: [1,2]', done => {
+      expect(filter_list([1,2,'a','b'])).to.equal([1,2]);
+      done();
+    })
+  })
+
+  describe('Input: [1,'a','b',0,15]', function() {
+    it('Expected Output: [1,0,15]', done => {
+      expect(filter_list([1,'a','b',0,15])).to.equal([1,0,15]);
+      done();
+    })
+  })
+
+  describe('Input: [1,2,'aasf','1','123',123]', function() {
+    it('Expected Output: [1,2,123]', done => {
+      expect(filter_list([1,2,'aasf','1','123',123])).to.equal([1,2,123]);
+      done();
+    })
+  })
+})
