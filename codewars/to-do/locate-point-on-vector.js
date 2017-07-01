22bb232c4632a179e3f537916764f3415c021e90
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-30 23:39:17 -0700

    progression in locate point on vector

--- codewars/to-do/locate-point-on-vector.js
+++ codewars/to-do/locate-point-on-vector.js
@@ -23,9 +23,15 @@ Your function must return:
 */
 
 function pointVsVector(point, vector) {
+  var origin = [0, 0]
   var vectorSlope = (vector[1][0] - vector[0][0]) / (vector[1][1] - vector[0][1]);
-  var xIntercept = [0, 0];
-  var pointSlope = point[0] /  point[1];
+
+  if (point[1] != 0) {
+    var pointSlope = point[0] /  point[1];
+  }
+  else {
+    var yIntercept = [point[0], 0];
+  }
 
   var bottomLtoTopR = false;
   var topLtoBottomR = false;
@@ -37,7 +43,12 @@ function pointVsVector(point, vector) {
   if (vector[1][0] > vector[0][0] && vector[1][1] > vector[0][1]) {
     bottomLtoTopR = true;
 
-    return pointSlope > vectorSlope ? 1: -1;
+    if (pointSlope) {
+      return pointSlope > vectorSlope ? 1: -1;
+    }
+    else {
+      return point[0] > vectorSlope ? 1: -1;
+    }
   }
 
   if (vector[1][0] < vector[0][0]  && vector[1][1] > vector[0][1]) {
@@ -46,43 +57,59 @@ function pointVsVector(point, vector) {
     return pointSlope < vectorSlope ? 1: -1;
   }
 
-  if (vector[1][0] < vector[0][0]  && vector[1][1] < vector[0][1]) {
-    bottomRtoTopL = true;
-  }
-
   if (vector[1][0] > vector[0][0]  && vector[1][1] < vector[0][1]) {
-    topRtoBottomL = true;
+    bottomRtoTopL = true;
+
+    return point[0] > 0 ? 1: -1;
   }
 
+  if (vector[1][0] < vector[0][0]  && vector[1][1] < vector[0][1]) {
+    topRtoBottomL = true;
 
+    return pointSlope < vectorSlope ? 1: -1;
+  }
 }
 
 describe('Locate Point in Relation to Vector', function() {
-  describe('with point [0,1] and vector [[0, 0], [1, 1]]', done => {
+  describe('Input (point is to left of positive sloping vector): P[0,1] & V[[0,0],[1,1]]', done => {
     it('should return -1', done => {
       expect(pointVsVector([0,1], [[0, 0], [1, 1]])).to.equal(-1);
       done();
     })
   })
 
-  // describe('with point [2,2] and vector [[0, 0], [1, 1]]', done => {
-  //   it('should return -1', done => {
-  //     expect(pointVsVector([2,2], [[0, 0], [1, 1]])).to.equal(0);
-  //     done();
-  //   })
-  // })
-  
-  describe('with point [2,0] and vector [[0, 0], [1, 1]]', done => {
-    it('should return -1', done => {
+  describe('Input (point is to right of negatively sloping vector): P[0,1] & V[[1,1],[0,0]]', done => {
+    it('should return 1', done => {
+      expect(pointVsVector([0,1], [[1, 1], [0, 0]])).to.equal(1);
+      done();
+    })
+  })
+
+  describe('Input (point is to right of positive sloping vector): P[2,0] & V[[0,0],[1,1]]', done => {
+    it('should return 1', done => {
       expect(pointVsVector([2,0], [[0, 0], [1, 1]])).to.equal(1);
       done();
     })
   })
-  //
-  // describe('with point [2,0] and vector [[0, 0], [3, 2]]', done => {
-  //   it('should return -1', done => {
-  //     expect(pointVsVector([2,0], [[0, 0], [3, 2]])).to.equal(1);
-  //     done();
-  //   })
-  // })
+
+  describe('Input (point lies on vector): P[2,2] & V[[0,0],[1,1]]', done => {
+    it('should return -1', done => {
+      expect(pointVsVector([2,2], [[0, 0], [1, 1]])).to.equal(0);
+      done();
+    })
+  })
+
+  describe('Input () [2,0] and vector [[0, 0], [3, 2]]', done => {
+    it('should return 1', done => {
+      expect(pointVsVector([2,0], [[0, 0], [3, 2]])).to.equal(1);
+      done();
+    })
+  })
+
+  describe('Input (point to right of northwest moving vector) P[1,0] & V[[1,-1],[0,0]]', done => {
+    it('should return 1', done => {
+      expect(pointVsVector([1,0], [[1, -1], [0, 0]])).to.equal(1);
+      done();
+    })
+  })
 })
