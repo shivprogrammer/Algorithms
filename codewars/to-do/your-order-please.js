7c36960f7de647245c3e57748b76849f2464572d
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-03 12:37:13 -0700

    your order please tests added

--- codewars/to-do/your-order-please.js
+++ codewars/to-do/your-order-please.js
@@ -13,3 +13,29 @@ If the input String is empty, return an empty String. The words in the input Str
 
 For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
 */
+
+function order(words){
+}
+
+describe('Your Order, Please | 6kyu', function() {
+  describe('Base Case Input: ""', function() {
+    it('Should return ""', done => {
+      expect(order('')).to.equal('');
+      done();
+    })
+  })
+
+  describe('Input: "is2 Thi1s T4est 3a"', function() {
+    it('Should return "Thi1s is2 3a T4est"', done => {
+      expect(order('is2 Thi1s T4est 3a')).to.equal('Thi1s is2 3a T4est');
+      done();
+    })
+  })
+
+  describe('Input: "4of Fo1r pe6ople g3ood th5e the2"', function() {
+    it('Should return "Fo1r the2 g3ood 4of th5e pe6ople"', done => {
+      expect(order('4of Fo1r pe6ople g3ood th5e the2')).to.equal('Fo1r the2 g3ood 4of th5e pe6ople');
+      done();
+    })
+  })
+})
