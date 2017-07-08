bff2a497ad5680b797820223f1fd2f0ffdb36db1
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-07 20:45:57 -0700

    all tests passing for who likes it!

--- codewars/to-do/who-likes-it.js
+++ codewars/to-do/who-likes-it.js
@@ -19,10 +19,31 @@ likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like
 */
 
 function likes(names) {
+  var extras = names.length - 2;
+  
+  if (names.length === 0) {
+    return 'no one likes this';
+  }
+
+  if (names.length === 1) {
+    return names[0] + ' likes this';
+  }
+
+  if (names.length === 2) {
+    return names[0] + ' and ' + names[1] + ' like this';
+  }
+
+  if (names.length === 3) {
+    return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+  }
+
+  if (names.length >= 4) {
+    return names[0] + ', ' + names[1] + ' and ' + extras + ' others like this';
+  }
 }
 
 describe('Who Likes It? | 6kyu', function() {
-  describe('Input: ["]', function() {
+  describe('Input: []', function() {
     it('should return: "no one likes this"', done => {
       expect(likes([])).to.equal('no one likes this');
       done();
@@ -50,16 +71,10 @@ describe('Who Likes It? | 6kyu', function() {
     })
   })
 
-  describe('Input: ['Alex', 'Jacob', 'Mark', 'Max']', function() {
+  describe('Input: ["Alex", "Jacob", "Mark", "Max"]', function() {
     it('should return: "Alex, Jacob and 2 others like this"', done => {
       expect(likes(['Alex', 'Jacob', 'Mark', 'Max'])).to.equal('Alex, Jacob and 2 others like this');
       done();
     })
   })
 })
-
-Test.assertEquals(likes([]), 'no one likes this');
-  Test.assertEquals(likes(['Peter']), 'Peter likes this');
-  Test.assertEquals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
-  Test.assertEquals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
-  Test.assertEquals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
