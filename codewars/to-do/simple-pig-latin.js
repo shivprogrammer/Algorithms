c5f30b1c5978dd7d6656fe73959b20619ae9fbdd
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-10 23:33:42 -0700

    holy shit solved simple pig latin on first try WHATTTTT

--- codewars/to-do/simple-pig-latin.js
+++ codewars/to-do/simple-pig-latin.js
@@ -7,6 +7,24 @@ Move the first letter of each word to the end of it, then add 'ay' to the end of
 pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 */
 
+function pigIt(str) {
+  var words = str.split(' ');
+  var temp = '';
+  var output = '';
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < 1; j++) {
+      if (j === 0) {
+        temp = words[i][j];
+        output += words[i].slice(1, words[i].length) + temp + 'ay ';
+        temp = '';
+      }
+    }
+  }
+
+  return output.slice(0, output.length - 1);
+}
+
 describe('Simple Pig Latin', function() {
   describe('Input: Pig Latin is cool', function() {
     it('Expected: igPay atinlay siay oolcay', done => {
