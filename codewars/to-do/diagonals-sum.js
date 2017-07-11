fc952cf2b2b832a50c45c55a27f5882ff890903b
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-10 22:02:28 -0700

    tests added for diagonals sum | 7kyu

--- codewars/to-do/diagonals-sum.js
+++ codewars/to-do/diagonals-sum.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+Diagonals Sum
+
 7 kyu
 
 Create a function that receives a (square) matrix and calculates the sum of both diagonals (main and secondary)
@@ -16,3 +20,27 @@ diagonals( [
 
 returns -> 20 // 1 + 5 + 9 + 3 + 5 + 7
 */
+
+describe('Matrix Diagonals Sum Question Code Wars', function() {
+  describe('Input: no input', function() {
+    it('Expected: 0', done => {
+      expect(sum()).to.equal(0);
+      done();
+    })
+  })
+
+  describe('Input: empty array', function() {
+    it('Expected: 0', done => {
+      expect(sum()).to.equal(0);
+      done();
+    })
+  })
+
+
+  describe('Input: [[1,2,3], [4,5,6], [7,8,9]])', function() {
+    it('Expected: 20', done => {
+      expect(sum([[1,2,3], [4,5,6], [7,8,9]])).to.equal(30)
+      done();
+    })
+  })
+})
