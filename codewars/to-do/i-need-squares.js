a4a6437e189038a6e46245e4c85fe329251cf64a
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-12 01:03:13 -0700

    long hand function added for i need squares

--- codewars/to-do/i-need-squares.js
+++ codewars/to-do/i-need-squares.js
@@ -17,7 +17,19 @@ Good luck! :D
 */
 
 function squaresOnly(array) {
-
+  var result = [], isSquare;
+  for (let i = 0; i < array.length; i++) {
+    isSquare = !1; // !1 evaluates to false and is therefore a shorthand way of typing 'false'
+    for (let k = 0; k <= 10; k++) {
+      if (k ** 2 === array[i]) {
+        isSquare = true;
+      }
+    }
+    if (isSquare) {
+      result[result.length] = array[i];
+    }
+  }
+  return result;
 }
 
 describe('I Need Squares | 8kyu', function() {
