5b6cd2136635c0d585004755bbb7b496be632bd6
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-12 01:02:40 -0700

    tests added for i need squares

--- codewars/to-do/i-need-squares.js
+++ codewars/to-do/i-need-squares.js
@@ -15,3 +15,44 @@ There are a lot of handy built-in Array methods in Javascript that you may have 
 Don't you think the array parameter is a bit wordy? ;)
 Good luck! :D
 */
+
+function squaresOnly(array) {
+
+}
+
+describe('I Need Squares | 8kyu', function() {
+  describe('Input: [0,1,2,3,4,5,6,7,8,9,10]', function() {
+    it('should return [0,1,4,9]', done => {
+      expect(squaresOnly([0,1,2,3,4,5,6,7,8,9,10])).to.deep.equal([0,1,4,9]);
+      done();
+    })
+  })
+
+  describe('Input: [31,33,35,37,39,38,36,34,32]', function() {
+    it('should return [36]', done => {
+      expect(squaresOnly([31,33,35,37,39,38,36,34,32])).to.deep.equal([36]);
+      done();
+    })
+  })
+
+  describe('Input: [0,10,20,30,40,50,60,70,80,90,100]', function() {
+    it('should return [0,100]', done => {
+      expect(squaresOnly([0,10,20,30,40,50,60,70,80,90,100])).to.deep.equal([0,100]);
+      done();
+    })
+  })
+
+  describe('Input: [20,19,18,17,16,15,13,11,9,1,5,4]', function() {
+    it('should return [16,9,1,4]', done => {
+      expect(squaresOnly([20,19,18,17,16,15,13,11,9,1,5,4])).to.deep.equal([16,9,1,4]);
+      done();
+    })
+  })
+
+  describe('Input: [45,36,75,64,9,9,4,1,4,1,1,16,16,18,17,35,25,35]', function() {
+    it('should return [36,64,9,9,4,1,4,1,1,16,16,25]', done => {
+      expect(squaresOnly([45,36,75,64,9,9,4,1,4,1,1,16,16,18,17,35,25,35])).to.deep.equal([36,64,9,9,4,1,4,1,1,16,16,25]);
+      done();
+    })
+  })
+})
