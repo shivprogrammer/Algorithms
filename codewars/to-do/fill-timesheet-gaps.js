f4db13518e03a129cc20dbc9888b46341c2a6a77
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-12 20:11:05 -0700

    started and instructions added for fill timesheet gaps

---
+++ codewars/to-do/fill-timesheet-gaps.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+Fill the Gaps in my timesheet
+
+6kyu
+
+Background:
+At work I need to keep a timesheet, by noting which project I was working on every 15 minutes. I have an timer that beeps every 15 minutes to prompt me to note down what I was working on at that point, but sometimes when I'm away from my desk or working continuously on one project, I don't note anything down and these get recorded as null.
+
+Task:
+
+Help me populate my timesheet by replacing any null values in the array with the correct project name which is given by surrounding matching values.
+
+Examples:
+
+fill_gaps([1,null,1]) -> [1,1,1]   # Replace nulll values surrounded by matching values
+fill_gaps([1,null,null,null,1]) -> [1,1,1,1,1]  # There may be multiple nulls
+fill_gaps([1,null,1,2,null,2]) -> [1,1,1,2,2,2]  # There may be multiple replacements required
+fill_gaps([1,null,2,null,2,null,1]) -> [1,null,2,2,2,null,1]  # No nesting.
+fill_gaps([1,null,2]) -> [1,null,2] # No replacement if ends don't match
+fill_gaps([null,1,null]) -> [null,1,null] # No replacement if ends don't match off the ends of the array
+fill_gaps(['codewars', null, null, 'codewars', 'real work', null, null, 'real work']) -> ["codewars", "codewars", "codewars", "codewars", "real work", "real work", "real work", "real work"] # Works with strings too
+*/
