199d442dad2f6de3f1d00f759b425ef9bb92fd6b
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-18 15:49:12 -0400

    function completed for sushi go round

--- codewars/to-do/sushi-go-round.js
+++ codewars/to-do/sushi-go-round.js
@@ -1,6 +1,10 @@
 'use strict';
 
 /*
+Sushi-go-round (Beginner's)
+
+7kyu
+
 Sam has opened a new sushi train restaurant - a restaurant where sushi is served on plates that travel around the bar on a conveyor belt and customers take the plate that they like.
 
 Sam is using Glamazon's new visual recognition technology that allows a computer to record the number of plates at a customer's table and the colour of those plates. The number of plates is returned as a string. For example, if a customer has eaten 3 plates of sushi on a red plate the computer will return the string 'rrr'.
@@ -20,3 +24,21 @@ Input: 'rr'  Output: 4
 Input: 'rr rrr' Output: 8
 Input: 'rrrrr rrrrr' Output: 16
 */
+
+function sushi (str) {
+  var output = 0;
+  var count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === 'r') {
+      count++;
+      if (!(count % 5 === 0)) {
+        output += 2;
+      }
+    }
+  }
+
+  return output;
+}
+
+describe('')
