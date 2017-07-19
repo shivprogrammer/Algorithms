00c7fb107958802c5aacbc904fb7d56be729c85b
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-19 18:08:51 -0400

    tests added for 3 and 5 multiples | 5kyu

--- codewars/to-do/3-and-5-multiples.js
+++ codewars/to-do/3-and-5-multiples.js
@@ -13,3 +13,19 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 be
 
 Note: If the number is a multiple of both 3 and 5, only count it once.
 */
+
+describe('Code Wars 3 and 5 multiples questions', function() {
+  describe('With an input of 10', function() {
+    it('should return 23', done => {
+      expect(solution(10)).to.equal(23);
+      done();
+    })
+  })
+
+  describe('With an input of 20', function() {
+    it('should return 78', done => {
+      expect(solution(20)).to.equal(78);
+      done();
+    })
+  })
+})
