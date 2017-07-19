677dc78def644947c099864f075253088522cd2b
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-19 18:01:38 -0400

    added cube volume difference tests

--- codewars/to-do/cube-volume-difference.js
+++ codewars/to-do/cube-volume-difference.js
@@ -12,3 +12,40 @@ For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of 
 Your function will be tested with pre-made examples as well as random ones.
 If you can, try writing it in one line of code.
 */
+
+describe('Cube Volume Difference Problem from Code Wars', function() {
+  describe('Input: [3, 2, 5], [1, 4, 4]', function() {
+    it('Expected: 14', done => {
+      expect(find_difference([3, 2, 5], [1, 4, 4])).to.equal(14);
+      done();
+    })
+  })
+
+  describe('Input: [9, 7, 2], [5, 2, 2]', function() {
+    it('Expected: 106', done => {
+      expect(find_difference([9, 7, 2], [5, 2, 2])).to.equal(106);
+      done();
+    })
+  })
+
+  describe('Input: [11, 2, 5], [1, 10, 8]', function() {
+    it('Expected: 30', done => {
+      expect(find_difference([11, 2, 5], [1, 10, 8])).to.equal(30);
+      done();
+    })
+  })
+
+  describe('Input: [4, 4, 7], [3, 9, 3]', function() {
+    it('Expected: 31', done => {
+      expect(find_difference([4, 4, 7], [3, 9, 3])).to.equal(31);
+      done();
+    })
+  })
+
+  describe('Input: [15, 20, 25], [10, 30, 25]', function() {
+    it('Expected: 0', done => {
+      expect(find_difference([15, 20, 25], [10, 30, 25])).to.equal(0);
+      done();
+    })
+  })
+})
