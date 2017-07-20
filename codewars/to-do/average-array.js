5372585441ac058f1a78b8e5c1a4e96c628df353
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-20 14:10:01 -0400

    average array  started, instructions added | 7kyu

---
+++ codewars/to-do/average-array.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+Average Array
+
+7kyu
+
+ASC Week 1 Challenge 5 (Medium #2)
+
+Create a function that takes a 2D array as an input, and outputs another array that contains the average values for the numbers in the nested arrays at the corresponding indexes.
+
+For Example:
+
+avgArray([[1, 2, 3, 4], [5, 6, 7, 8]]); // => [3, 4, 5, 6]
+avgArray([[2, 3, 9, 10, 7], [12, 6, 89, 45, 3], [9, 12, 56, 10, 34], [67, 23, 1, 88, 34]]); // => [22.5, 11, 38.75, 38.25, 19.5]
+*/
