01d566abf7dbe7431d43cd0cada2483223285489
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-25 14:30:15 -0400

    tests written for band name generator

--- codewars/to-do/band-name-generator.js
+++ codewars/to-do/band-name-generator.js
@@ -17,3 +17,36 @@ europe -> Europeurope
 
 Can you write a function that takes in a noun as a string, and returns her preferred band name written as a string?
 */
+
+function bandNameGenerator(str) {
+}
+
+describe('Band Name Generator | 7kyu', function() {
+  describe('Input: "knife"', function() {
+    it('should return "The Knife"', done => {
+      expect(bandNameGenerator('knife')).to.equal('The Knife');
+      done();
+    })
+  })
+
+  describe('Input: "tart"', function() {
+    it('should return "Tartart"', done => {
+      expect(bandNameGenerator('tart')).to.equal('Tartart');
+      done();
+    })
+  })
+
+  describe('Input: "sandles"', function() {
+    it('should return "Sandlesandles"', done => {
+      expect(bandNameGenerator('sandles')).to.equal('Sandlesandles');
+      done();
+    })
+  })
+
+  describe('Input: "bed"', function() {
+    it('should return "The Bed"', done => {
+      expect(bandNameGenerator('bed')).to.equal('The Bed');
+      done();
+    })
+  })
+})
