8b066f41309a54fa75c95452ba5027fc8f5b4b6a
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-25 14:40:49 -0400

    started product of array items | 7kyu

---
+++ codewars/to-do/product-of-array-items.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+Product of Array Items
+
+7kyu
+
+Calculate the product of all elements in an array.
+
+In JavaScript, if the array is null or is empty, the function should return null.
+
+As a challenge, try writing your method in just one line of code. It's possible to have only 36 characters within your method.
+*/
