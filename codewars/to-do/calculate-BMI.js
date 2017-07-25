2e4ea1b24bbbb1b82d8350249c4cf9c25c1f6313
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-25 18:12:32 -0400

    test written for calculate bmi

--- codewars/to-do/calculate-BMI.js
+++ codewars/to-do/calculate-BMI.js
@@ -7,5 +7,14 @@ Calculate BMI
 */
 
 function bmi(weight, height) {
-  
+
 }
+
+describe('Calculate BMI | 8kyu', function() {
+  describe('Weight: 80, height: 1.80', function() {
+    it('should return normal', done => {
+      expect(bmi(80, 1.80)).to.equal('normal');
+      done();
+    })
+  })
+})
