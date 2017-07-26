4c47154052038e5a705f2ebe3f64b16260e3c525
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-26 10:31:03 -0400

    tests added for camel case

--- codewars/to-do/break-camel-case.js
+++ codewars/to-do/break-camel-case.js
@@ -14,3 +14,12 @@ solution('camelCasing') // => should return 'camel Casing'
 
 function solution(string) {
 }
+
+describe('Break Camel Case Code Wars', function() {
+  describe('if input is camelCase', function() {
+    it('output to be camel Case', done => {
+      expect(solution('camelCase')).to.equal('camel Case');
+      done();
+    })
+  })
+})
