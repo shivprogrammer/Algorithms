8dac46853d87df960602bbe4ae105018dba44d11
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-27 02:09:40 -0400

    all tests passing for is uniquegit add .

--- cracking-the-coding-interview/1-arrays-and-strings.js/1.1-is-unique.js
+++ cracking-the-coding-interview/1-arrays-and-strings.js/1.1-is-unique.js
@@ -9,7 +9,17 @@ Implement an algorithm to determine if a string has all unique characters. What 
 */
 
 function isUnique(str) {
+  var uniqueMap = new Map();
 
+  for (let i = 0; i < str.length; i++) {
+    if (uniqueMap.has(str.charAt(i))) {
+      return false;
+    }
+    else {
+      uniqueMap.set(str.charAt(i), 1);
+    }
+  }
+  return true;
 }
 
 describe('1.1 Is Unique | Cracking the Coding Interview | Chapter 1 - Arrays and Strings', function() {
