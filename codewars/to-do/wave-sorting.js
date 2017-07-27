40563758d2b380bf94eb33e5dc7bd0a85ee77c00
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-27 01:41:08 -0400

    test added for wave sort

--- codewars/to-do/wave-sorting.js
+++ codewars/to-do/wave-sorting.js
@@ -9,3 +9,12 @@ Wave Sorting
 function waveSort(arr) {
 
 }
+
+describe('Wave Sorting | 6kyu', function() {
+  describe('Input: [1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4]', function() {
+    it('should return false', done => {
+      expect(waveSort([1, 2, 34, 4, 5, 5, 5, 65, 6, 65, 5454, 4])).to.equal(false);
+      done();
+    })
+  })
+})
