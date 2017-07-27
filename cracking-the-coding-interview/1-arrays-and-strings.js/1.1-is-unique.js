bd43c60734822e102a349c75e1ef203189c486d2
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-27 02:07:19 -0400

    tests written for is unique

--- cracking-the-coding-interview/1-arrays-and-strings.js/1.1-is-unique.js
+++ cracking-the-coding-interview/1-arrays-and-strings.js/1.1-is-unique.js
@@ -1,7 +1,36 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Is Unique:
 
 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 */
+
+function isUnique(str) {
+
+}
+
+describe('1.1 Is Unique | Cracking the Coding Interview | Chapter 1 - Arrays and Strings', function() {
+  describe('input is empty string', function() {
+    it('should return true', done => {
+      expect(isUnique('')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: "abcdefg"', function() {
+    it('should return true', done => {
+      expect(isUnique('abcdefg')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: "abcdegg"', function() {
+    it('should return false', done => {
+      expect(isUnique('abcdegg')).to.equal(false);
+      done();
+    })
+  })
+})
