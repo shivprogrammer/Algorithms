51e18fc7c332e5d736810400d0e596ae527be24a
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-27 23:58:31 -0400

    2/3 tests passing for 16.2 word frequencies

--- cracking-the-coding-interview/16-moderate/16.2/16.2-word-frequencies.js
+++ cracking-the-coding-interview/16-moderate/16.2/16.2-word-frequencies.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 16.2 Word Frequencies
 
@@ -7,29 +9,48 @@ Design a method to find the frequency of occurrences of any given word in a book
 */
 
 function wordFreq(book, word) {
+  var bookWords = book.split(' ');
 
+  var bookMap = new Map();
+  for (let i = 0; i < bookWords.length; i++) {
+    if (bookMap.has(bookWords[i])) {
+      bookMap.set(bookWords[i], bookMap.get(bookWords[i]) + 1);
+    }
+    else {
+      bookMap.set(bookWords[i], 1);
+    }
+  }
+
+  if (bookMap.has(word)) {
+    return bookMap.get(word);
+  }
+  else {
+    return 0;
+  }
 }
 
-var book = 'So this is the tale of a land that was far, far away, where a crazy young gentlemen was working his ass off to be the best programmer that he could be. Despite the trials and tribulations, he knew that there was a shitload of money involved, and he grew to enjoy this meticulous art the more time he spent on it.';
+var aBook = 'So this is the tale of a land that was far, far away, where a crazy young gentlemen was working his ass off to be the best programmer that he could be. Despite the trials and tribulations, he knew that there was a shitload of money involved, and he grew to enjoy this meticulous art the more time he spent on it.';
+
+// console.log(wordFreq(aBook, 'titties'));
 
 describe('16.2 Word Frequences | Cracking the Coding Interview | Chapter 16 - Moderate', function() {
-  describe('Input: book, "programmer"', function() {
+  describe('Input: aBook, "programmer"', function() {
     it('should return 1', done => {
-      expect(wordFreq(book, 'programmer')).to.equal(1);
+      expect(wordFreq(aBook, 'programmer')).to.equal(1);
       done();
     })
   })
 
-  describe('Input: book, "far"', function() {
+  describe('Input: aBook, "far"', function() {
     it('should return 2', done => {
-      expect(wordFreq(book, 'far')).to.equal(2);
+      expect(wordFreq(aBook, 'far')).to.equal(2);
       done();
     })
   })
 
-  describe('Input: book, "bruh"', function() {
+  describe('Input: aBook, "bruh"', function() {
     it('should return 0', done => {
-      expect(wordFreq(book, 'bruh')).to.equal(0);
+      expect(wordFreq(aBook, 'bruh')).to.equal(0);
       done();
     })
   })
