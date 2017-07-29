c77d69c5f87e6ad106de9b5e10650f826b596a3e
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-29 16:44:48 -0400

    test added for 16.6 smallest difference

--- cracking-the-coding-interview/16-moderate/16.6/smallest-difference.js
+++ cracking-the-coding-interview/16-moderate/16.6/smallest-difference.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Smallest Difference
 
@@ -7,5 +9,13 @@ Givene two arrays of integers, compute the pair of values (one value in each arr
 */
 
 function smallDif(arr1, arr2) {
-
 }
+
+describe('16.6 Smallest Difference | Cracking the Coding Interview | Chapter 16 -- Moderate', function() {
+  describe('Input: [1, 3, 15, 11, 2], [23, 127, 235, 19, 8]', function() {
+    it('should return [11, 8]', done => {
+      expect(smallDif([1, 3, 15, 11, 2], [23, 127, 235, 19, 8])).to.deep.equal([11, 8]);
+      done();
+    })
+  })
+})
