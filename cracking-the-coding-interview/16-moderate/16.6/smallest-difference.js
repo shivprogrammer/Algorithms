09c39ce69710a46c183469cf9baca84b8db1348f
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-29 17:00:13 -0400

    added verbal solution for 16.6 smallest diff, note to add sorted solution

--- cracking-the-coding-interview/16-moderate/16.6/smallest-difference.js
+++ cracking-the-coding-interview/16-moderate/16.6/smallest-difference.js
@@ -27,6 +27,8 @@ function smallDif(arr1, arr2) {
 // Time Complexity: O(N squared) [nested for loops]
 // Space Complexity: O(M) [output]
 
+// -- THERE IS A MORE EFFICIENT ALGORITHM THAT NEEDS TO BE CODED, WHICH IS BY SORTING THE ARRAYS FIRST -- 
+
 describe('16.6 Smallest Difference | Cracking the Coding Interview | Chapter 16 -- Moderate', function() {
   describe('Input: [1, 3, 15, 11, 2], [23, 127, 235, 19, 8]', function() {
     it('should return [11, 8]', done => {
---
+++ cracking-the-coding-interview/16-moderate/16.6/smallest-differnce.java
@@ -0,0 +1,9 @@
+/*
+16.6 -- Smallest Difference
+
+Given two arrays of integers, compute the pair of values (one value in each array) with the smallest (non-negative) difference. Retur7u8n the difference.
+
+Brute force method // was what I came up with and correctly programmed
+
+A more optimal approach is to sort the arrays. Once the arrays are sorted, we can find the minimum differene by iterating through the array.
+*/
