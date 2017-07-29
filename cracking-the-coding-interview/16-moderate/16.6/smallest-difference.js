800bfa1f058c29d7fee6f07742423b53a0d897ed
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-29 16:40:59 -0400

    started 16.6 smallest difference | cracking the coding interview

---
+++ cracking-the-coding-interview/16-moderate/16.6/smallest-difference.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+Smallest Difference
+
+Givene two arrays of integers, compute the pair of values (one value in each array) with the smallest (non-negative integer) difference. Return the difference
+*/
+
+function smallDif(arr1, arr2) {
+
+}
