83c0bba5c487d97dc91f2e3139d0a7269da58792
shivprogrammer <shiv.unadkat@gmail.com>
2017-07-31 15:43:09 -0400

    test passing for array diff

--- codewars/to-do/array-diff.js
+++ codewars/to-do/array-diff.js
@@ -15,6 +15,9 @@ difference([1,2,2,2,3],[2]) == [1,3]
 */
 
 function array_diff(a, b) {
+  return a.filter(function(i) {
+    return b.indexOf(i) < 0;
+  })
 }
 
 describe('Array Diff Question from Code Wars', function() {
