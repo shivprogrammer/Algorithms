7a2d175565a2ef14f47d289306f7f97f3cff58d7
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-04 16:19:03 -0700

    started even or odd | 8kyu

---
+++ codewars/to-do/even-or-odd.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+Even Or Odd
+
+8kyu
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+function even_or_odd(number) {
+
+}
