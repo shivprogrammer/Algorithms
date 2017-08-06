ad7ed98ea8d685e2d634dd290c2af1511066b095
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-05 18:59:18 -0700

    tests added for supersize

--- codewars/to-do/supersize.js
+++ codewars/to-do/supersize.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Write a function that rearranges an integer into its largest possible value.
 
@@ -12,3 +14,76 @@ If the argument passed through is single digit or is already the maximum possibl
 function superSize(num){
 
 }
+
+
+describe('Supersize function Code Wars', function() {
+  describe('Input: ', function() {
+    it('Expect: NaN', done => {
+      expect(superSize()).to.deep.equal(NaN);
+      done();
+    })
+  })
+
+  describe('Input: 69', function() {
+    it('Expect: 96', done => {
+      expect(superSize(69)).to.equal(96);
+      done();
+    })
+  })
+
+  describe('Input: 513', function() {
+    it('Expect: 531', done => {
+      expect(superSize(513)).to.equal(531);
+      done();
+    })
+  })
+
+  describe('Input: 2017', function() {
+    it('Expect: 7210', done => {
+      expect(superSize(2017)).to.equal(7210);
+      done();
+    })
+  })
+
+  describe('Input: 414', function() {
+    it('Expect: 441', done => {
+      expect(superSize(414)).to.equal(441);
+      done();
+    })
+  })
+
+  describe('Input: 608719', function() {
+    it('Expect: 987610', done => {
+      expect(superSize(608719)).to.equal(987610);
+      done();
+    })
+  })
+
+  describe('Input: 123456789', function() {
+    it('Expect: 987654321', done => {
+      expect(superSize(123456789)).to.equal(987654321);
+      done();
+    })
+  })
+
+  describe('Input: 700000000001', function() {
+    it('Expect: 710000000000', done => {
+      expect(superSize(700000000001)).to.equal(710000000000);
+      done();
+    })
+  })
+
+  describe('Input: 666666', function() {
+    it('Expect: 666666', done => {
+      expect(superSize(666666)).to.equal(666666);
+      done();
+    })
+  })
+
+  describe('Input: 2', function() {
+    it('Expect: 2', done => {
+      expect(superSize(2)).to.equal(2);
+      done();
+    })
+  })
+})
