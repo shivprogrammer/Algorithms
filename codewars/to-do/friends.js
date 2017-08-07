c05043cac5360b1562312a074826b13bdb43e46a
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-06 23:13:55 -0700

    tests added for friends

--- codewars/to-do/friends.js
+++ codewars/to-do/friends.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect
+
 /*
 Friends
 
@@ -12,3 +14,33 @@ Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 
 function friend(friends) {
 }
+
+describe('Friends Problem From Code Wars', function() {
+  describe('Input: ["Ryan", "Kieran", "Mark"]', function() {
+    it('Expected: ["Ryan", "Mark"]', done => {
+      expect(friend(["Ryan", "Kieran", "Mark"])).to.deep.equal(["Ryan", "Mark"]);
+      done();
+    })
+  })
+
+  describe('Input: ["Ryan", "Jimmy", "123", "4", "Cool Man"]', function() {
+    it('Expected: ["Ryan"]', done => {
+      expect(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])).to.deep.equal(["Ryan"]);
+      done();
+    })
+  })
+
+  describe('Input: ["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]', function() {
+    it('Expected: ["Jimm", "Cari", "aret"]', done => {
+      expect(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"])).to.deep.equal(["Jimm", "Cari", "aret"]);
+      done();
+    })
+  })
+
+  describe('Input: ["Love", "Your", "Face", "1"]', function() {
+    it('Expected: ["Love", "Your", "Face"]', done => {
+      expect(friend(["Love", "Your", "Face", "1"])).to.deep.equal(["Love", "Your", "Face"]);
+      done();
+    })
+  })
+})
