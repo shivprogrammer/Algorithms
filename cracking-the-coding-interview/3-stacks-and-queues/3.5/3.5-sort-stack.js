9c66bd0ad55d14b630eb9c47bb2422079d442152
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-10 07:56:58 -0400

    progressing on the 3.5 sort stack

--- cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
+++ cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
@@ -9,6 +9,23 @@ Write a program to sort a stack such that the smallest items are on top. You can
 */
 
 function sortStack(input) {
+  var stack1 = [];
+  var stack2 = [];
+  var high = 0;
+  var low = Infinity;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > high) {
+      high = input[i];
+    }
+    if (input[i] < low) {
+      low = input[i];
+    }
+  }
+
+  console.log(low, high);
+
+  
 }
 
 describe('3.5 - Sort Stack', function() {
