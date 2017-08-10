9a785e341910398c5fec2a252e9ead481b9a1025
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-10 09:11:43 -0400

    test written for 1.7 rotate matrix

--- cracking-the-coding-interview/1-arrays-and-strings.js/1.7/1.7-rotate-matrix.js
+++ cracking-the-coding-interview/1-arrays-and-strings.js/1.7/1.7-rotate-matrix.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 1.7 Rotate Matrix
 
@@ -9,3 +11,22 @@ Given an image represented by an NxN matrix, where each pixel in the image is 4 
 function rotate(matrix) {
 
 }
+
+var matrix1 = [
+  [4, 12],
+  [8, 5]
+];
+
+var matrix1rotated = [
+  [8, 4],
+  [5, 12]
+];
+
+describe('1.7 Rotate Matrix | Cracking the Coding Interview | Chapter 1 - Arrays and Strings', function() {
+  describe('2x2 Matrix Input: matrix1', function() {
+    it('should return matrix1rotated', done => {
+      expect(rotate(matrix1)).to.deep.equal(matrix1rotated);
+      done();
+    })
+  })
+})
