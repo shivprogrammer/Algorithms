6d8097a897c41faedf51daa686d5ff8997593c45
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-10 07:05:23 -0400

    started 3.5 sort stack | cracking the coding interview | chapter 3 - stacks and queues

---
+++ cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+3.5 Sort Stack
+
+Write a program to sort a stack such that the smallest items are on top. You can use an additional empty stack, but you may not copy the elements into any other data structure (such as an array). The stack supports the following operations: push, pop, peek, and isEmpty.
+*/
+
+function sortStack(input) {
+  
+}
