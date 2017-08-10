eb23de7fe08850d259b6ea9cd6f6da5456a69dec
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-10 07:40:59 -0400

    test written for 3.5 sort stack

--- cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
+++ cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 3.5 Sort Stack
 
@@ -7,5 +9,13 @@ Write a program to sort a stack such that the smallest items are on top. You can
 */
 
 function sortStack(input) {
-  
 }
+
+describe('3.5 - Sort Stack', function() {
+  describe('Input: [2, 12, 8, 7, 5, 4, 10]', function() {
+    it('should return []', done => {
+      expect(sortStack([2, 12, 8, 7, 5, 4, 10])).to.deep.equal([2, 4, 5, 7, 8, 10, 12]);
+      done();
+    })
+  })
+})
