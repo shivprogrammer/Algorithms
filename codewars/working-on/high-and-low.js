382b8cc91979d063c5d6babd3b28b05ff6b6f7e7
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-11 22:13:56 -0700

    tests added for high and low

--- codewars/working-on/high-and-low.js
+++ codewars/working-on/high-and-low.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 High and Low
 
@@ -15,3 +17,26 @@ highAndLow("1 9 3 4 -5"); // return "9 -5"
 
 function highAndLow(nums) {
 }
+
+describe('Find Highest and Lowest Int Question Code Wars', function() {
+  describe('Input: "1 9 3 4 -5"', function() {
+    it('Expected: "9 -5"', done => {
+      expect(highAndLow("1 9 3 4 -5")).to.equal("9 -5");
+      done();
+    })
+  })
+
+  describe('Input: "1 2 -3 4 5"', function() {
+    it('Expected: "5 -3"', done => {
+      expect(highAndLow("1 2 -3 4 5")).to.equal("5 -3");
+      done();
+    })
+  })
+
+  describe('Input: "1111 222 -124"', function() {
+    it('Expected: "1111 -124"', done => {
+      expect(highAndLow("1111 222 -124")).to.equal("1111 -124");
+      done();
+    })
+  })
+})
