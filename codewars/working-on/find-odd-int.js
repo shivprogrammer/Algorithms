b0523e83b9f34e74ae11955c7ba548568667d01e
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-13 20:24:51 -0700

    find odd int tests added

--- codewars/working-on/find-odd-int.js
+++ codewars/working-on/find-odd-int.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Find the Odd Int
 
@@ -12,3 +14,19 @@ There will always be only one integer that appears an odd number of times.
 function findOdd(arr) {
 
 }
+
+describe('Find Odd Int Question Code Wars', function() {
+  describe('with input [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]', function() {
+    it('should return 5', done => {
+      expect(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])).to.equal(5);
+      done();
+    })
+  })
+
+  describe('with input [1,1,2,-2,5,2,4,4,-1,-2,5]', function() {
+    it('should return -1', done => {
+      expect(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])).to.equal(-1);
+      done();
+    })
+  })
+})
