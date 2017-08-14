403543b5d1e4c708422aa572856d1c386668ae5a
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-13 20:24:20 -0700

    started find odd int | 6kyu

---
+++ codewars/working-on/find-odd-int.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+Find the Odd Int
+
+6kyu
+
+Given an array, find the int that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+*/
+
+function findOdd(arr) {
+
+}
