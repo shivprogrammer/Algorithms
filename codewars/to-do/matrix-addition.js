0e517dc9d9eb819fbe83ee65e93439c105075ba2
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-18 17:54:16 -0400

    all tests passing for matrix addition my dude ayooo

--- codewars/to-do/matrix-addition.js
+++ codewars/to-do/matrix-addition.js
@@ -26,7 +26,24 @@ matrixAddition( [ [1, 2, 3], [3, 2, 1,], [1, 1, 1] ], [ [2, 2, 1], [3, 2, 3], [1
 returns [ [3, 4, 4], [6, 4, 4], [2, 2, 4] ]
 */
 
-function matrixAddition(a, b){
+function matrixAddition(matrix1, matrix2) {
+  if (matrix1.length != matrix2.length && matrix1[0].length != matrix2[0].length) {
+    return false;
+  }
+
+  var outputMatrix = [];
+  var temp = [];
+
+  for (let i = 0; i < matrix1.length; i++) {
+    for (let j = 0; j < matrix1[0].length; j++) {
+      var cell = matrix1[i][j] + matrix2[i][j];
+      temp.push(cell);
+    }
+    outputMatrix.push(temp);
+    temp = [];
+  }
+
+  return outputMatrix;
 }
 
 var matrix1 = [
@@ -44,10 +61,35 @@ var matrix1plus2 = [
   [3, 5]
 ];
 
+var matrix3 = [
+  [1, 2, 3],
+  [3, 2, 1],
+  [1, 1, 1]
+];
+
+var matrix4 = [
+  [2, 2, 1],
+  [3, 2, 3],
+  [1, 1, 3]
+];
+
+var matrix3plus4 = [
+  [3, 4, 4],
+  [6, 4, 4],
+  [2, 2, 4]
+];
+
 describe('Matrix Addition | Code Wars | 6kyu', function() {
   describe('Input: matrix1 & matrix2', function() {
     it('should return matrix1plus2', done => {
       expect(matrixAddition(matrix1, matrix2)).to.deep.equal(matrix1plus2);
+      done();
+    })
+  })
+
+  describe('Input: matrix3 & matrix4', function() {
+    it('should return matrix3plus4', done => {
+      expect(matrixAddition(matrix3, matrix4)).to.deep.equal(matrix3plus4);
       done();
     })
   })
