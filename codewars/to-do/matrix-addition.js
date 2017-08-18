bf8e9b2554bed4ae9c006e823bef2351f929e7ae
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-18 15:10:19 -0400

    test added for matrix addition

--- codewars/to-do/matrix-addition.js
+++ codewars/to-do/matrix-addition.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Write a function that accepts two square matrices (nxn two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size nxn (square), containing only integers.
 
@@ -26,3 +28,27 @@ returns [ [3, 4, 4], [6, 4, 4], [2, 2, 4] ]
 
 function matrixAddition(a, b){
 }
+
+var matrix1 = [
+  [1, 2],
+  [1, 2]
+];
+
+var matrix2 = [
+  [2, 3],
+  [2, 3]
+];
+
+var matrix1plus2 = [
+  [3, 5],
+  [3, 5]
+];
+
+describe('Matrix Addition | Code Wars | 6kyu', function() {
+  describe('Input: matrix1 & matrix2', function() {
+    it('should return matrix1plus2', done => {
+      expect(matrixAddition(matrix1, matrix2)).to.deep.equal(matrix1plus2);
+      done();
+    })
+  })
+})
