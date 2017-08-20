a05480faf35c67879e1c7148d4800ed07b541e69
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-19 22:28:17 -0700

    odd sort test added

--- codewars/working-on/odd-sort.js
+++ codewars/working-on/odd-sort.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Sort the Odd
 
@@ -16,5 +18,14 @@ sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
 */
 
 function sortArray(arr) {
-  
+
 }
+
+describe('Odd Sort Code Wars', function() {
+  describe('Input: [5, 3, 2, 8, 1, 4]', function() {
+    it('Should return: [1, 3, 2, 8, 5, 4]', done => {
+      expect(sortArray([5, 3, 2, 8, 1, 4])).to.deep.equal([1, 3, 2, 8, 5, 4]);
+      done();
+    })
+  })
+})
