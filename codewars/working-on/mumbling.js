a6b9cfe35debbf94ccb1e2d1afcd41541f787702
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-20 19:58:02 -0700

    started mumbling | 7kyu

---
+++ codewars/working-on/mumbling.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+Mumbling
+
+7 kyu
+
+This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+accum("abcd");    // "A-Bb-Ccc-Dddd"
+accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt");    // "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+function accum(s) {
+
+}
