04ea90c739b99e6ee05c606af8fc12cf5bdb7002
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-21 23:33:21 -0400

    started 16.9 - Operations | Cracking the Coding Interview | Chapter 16 - Moderate

---
+++ cracking-the-coding-interview/16-moderate/16.9/16.9-operations.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+16.9 Operations
+
+Write methods to implement the multiply, subtract, and divide operations for integers. The result of all of these are integers. Use only the add operator.
+*/
+
+function operations(num1, num2, method) {
+  
+}
