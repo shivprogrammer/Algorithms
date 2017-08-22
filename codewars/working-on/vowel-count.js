71d7c910e4ab117afc79a89c09a169f0dda38ba3
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-21 22:33:18 -0700

    started vowel count | Code Wars - 7kyu

---
+++ codewars/working-on/vowel-count.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+Vowels count
+
+7yu
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+*/
+
+function getCount(str) {
+
+}
