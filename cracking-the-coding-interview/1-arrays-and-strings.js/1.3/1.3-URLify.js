f3d2dfc7d437015fa62c038bf010e7008d8e949b
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-22 01:02:08 -0400

    started 1.3 Replace Spaces | Cracking the Coding Interview | Chapter 1 - Arrays and Strings

---
+++ cracking-the-coding-interview/1-arrays-and-strings.js/1.3/1.3-URLify.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+1.3 URLIfy
+
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and you are given the "true" length of the string.
+*/
+
+function replaceSpaces(str) {
+
+}
