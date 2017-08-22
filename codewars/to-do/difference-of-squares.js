a0253ab2fb016931246ad89a3e76e646b7dad06e
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-22 02:04:08 -0400

    started difference of squares | 7kyu | Code Wars

---
+++ codewars/to-do/difference-of-squares.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+Find the difference between the sum of the squares of the first n natural numbers (1 <= n <= 100) and the square of their sum. For example, when n = 10:
+
+The square of the sum of the first ten natural numbers is:
+
+(1 + 2 + ... + 10)**2 = 55**2 = 3025
+
+The sum of the squares of first ten natural numbers is :
+
+1**2 + 2**2 + ... + 10**2 = 385
+
+Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbes is 3025 - 385 = 2640.
+
+Example
+
+differenceOfSquares(10) // => 2640
+*/
+
+function differenceOfSquares(x){
+}
