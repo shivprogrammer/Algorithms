4aa84dbbc489bb2d417f2357df1c606b7a85310b
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-22 10:21:31 -0400

    all tests passing, easy q but good warm up for beast mode

--- codewars/to-do/esreveR.js
+++ codewars/to-do/esreveR.js
@@ -11,20 +11,26 @@ Write a function reverse which reverses a list (or in clojure's case, any list-l
 */
 
 function reverse(array) {
+  var output = [];
 
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+
+  return output;
 }
 
 describe('esreveR | Code Wars | 7kyu', function() {
   describe('Input: [1, 2, 3, 4, 5]', function() {
     it('should return [5, 4, 3, 2, 1]', done => {
-      expecct(reverse([1, 2, 3, 4, 5])).to.deep.equal([5, 4, 3, 2, 1]);
+      expect(reverse([1, 2, 3, 4, 5])).to.deep.equal([5, 4, 3, 2, 1]);
       done();
     })
   })
 
   describe('Input: ["one", 455, "candles", 0, null]', function() {
     it('should return [null, 0, "candles", 455, "one"]', done => {
-      expecct(reverse(['one', 455, 'candles', 0, null])).to.deep.equal([null, 0, 'candles', 455, 'one']);
+      expect(reverse(['one', 455, 'candles', 0, null])).to.deep.equal([null, 0, 'candles', 455, 'one']);
       done();
     })
   })
