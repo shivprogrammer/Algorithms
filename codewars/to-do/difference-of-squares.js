e760bb457d0a58161f8be55eedef2c9d97c8cb36
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-22 02:07:51 -0400

    tests written for differences in squares

--- codewars/to-do/difference-of-squares.js
+++ codewars/to-do/difference-of-squares.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 Find the difference between the sum of the squares of the first n natural numbers (1 <= n <= 100) and the square of their sum. For example, when n = 10:
 
@@ -18,5 +20,28 @@ Example
 differenceOfSquares(10) // => 2640
 */
 
-function differenceOfSquares(x){
+function differenceOfSquares(x) {
 }
+
+describe('Difference of Squares | Code Wars | 7kyu', function() {
+  describe('Input: 5', function() {
+    it('should return 170', done => {
+      expect(differenceOfSquares(5)).to.equal(170);
+      done();
+    })
+  })
+
+  describe('Input: 10', function() {
+    it('should return 2640', done => {
+      expect(differenceOfSquares(10)).to.equal(2640);
+      done();
+    })
+  })
+
+  describe('Input: 100', function() {
+    it('should return 25164150', done => {
+      expect(differenceOfSquares(100)).to.equal(25164150);
+      done();
+    })
+  })
+})
