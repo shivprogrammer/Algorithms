15d5fc8f405b19accb2a3302e259ec4d354636d2
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-21 23:11:15 -0400

    all test passing on the first try songit add .git add .

--- cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
+++ cracking-the-coding-interview/3-stacks-and-queues/3.5/3.5-sort-stack.js
@@ -13,6 +13,8 @@ function sortStack(input) {
   var stack2 = [];
   var high = 0;
   var low = Infinity;
+  var temp;
+  var popCount = 0;
 
   for (let i = 0; i < input.length; i++) {
     if (input[i] > high) {
@@ -23,13 +25,52 @@ function sortStack(input) {
     }
   }
 
-  console.log(low, high);
+  for (let j = 0; j < input.length; j++) {
+    if (input[j] === low) {
+      stack1.push(input[j]);
+    }
+    else {
+      stack2.push(input[j]);
+    }
+  }
+
+  temp = stack2.pop();
+
+  while (stack1.length != input.length) {
+    while (temp < stack1[stack1.length - 1]) {
+      stack2.push(stack1.pop());
+      popCount++;
+    }
+    stack1.push(temp);
+
+    while (popCount) {
+      stack1.push(stack2.pop());
+      popCount--;
+    }
+    temp = stack2.pop();
+  }
+
+  return stack1;
 }
 
 describe('3.5 - Sort Stack', function() {
   describe('Input: [2, 12, 8, 7, 5, 4, 10]', function() {
-    it('should return []', done => {
+    it('should return [2, 4, 5, 7, 8, 10, 12]', done => {
       expect(sortStack([2, 12, 8, 7, 5, 4, 10])).to.deep.equal([2, 4, 5, 7, 8, 10, 12]);
+      done();
+    })
+  })
+
+  describe('Input: [15, -4, 3, 12, 8]', function() {
+    it('should return [-4, 3, 8, 12, 15]', done => {
+      expect(sortStack([15, -4, 3, 12, 8])).to.deep.equal([-4, 3, 8, 12, 15]);
+      done();
+    })
+  })
+
+  describe('Input with Repeat Numbers: [15, -4, 12, 3, 12, 8, 8]', function() {
+    it('should return [-4, 3, 8, 8, 12, 12, 15]', done => {
+      expect(sortStack([15, -4, 12, 3, 12, 8, 8])).to.deep.equal([-4, 3, 8, 8, 12, 12, 15]);
       done();
     })
   })
