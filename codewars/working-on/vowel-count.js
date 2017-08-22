4e0f16a2383b231639abaa51ca9e442f6d199543
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-21 22:44:04 -0700

    refactored vowel count to have time complexity go from O(N squared) to O(N)

--- codewars/working-on/vowel-count.js
+++ codewars/working-on/vowel-count.js
@@ -13,21 +13,24 @@ We will consider a, e, i, o, and u as vowels for this Kata.
 */
 
 function getCount(str) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var vowelsCount = 0;
+  var vowelMap = new Map();
+  vowelMap.set('a', 1);
+  vowelMap.set('e', 1);
+  vowelMap.set('i', 1);
+  vowelMap.set('o', 1);
+  vowelMap.set('u', 1);
+
+  var vowelCount = 0;
 
   for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < vowels.length; j++) {
-      if (str[i] === vowels[j]) {
-        vowelsCount++;
-        break;
-      }
+    if (vowelMap.has(str.charAt(i))) {
+      vowelCount++;
     }
   }
-  return vowelsCount;
+  return vowelCount;
 }
 
-// Time Complexity: O(N * M)
+// Time Complexity: O(N)
 // Space Complexity: O(M)
 
 describe('Vowels Count Code Wars 7kyu', function() {
