8a1a2847440fdf3f98a7a3e5c6bb76b8e724b988
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-22 01:12:29 -0400

    all tests passing for 1.3 URLifygit add .

--- cracking-the-coding-interview/1-arrays-and-strings.js/1.3/1.3-URLify.js
+++ cracking-the-coding-interview/1-arrays-and-strings.js/1.3/1.3-URLify.js
@@ -9,13 +9,31 @@ Write a method to replace all spaces in a string with '%20'. You may assume that
 */
 
 function replaceSpaces(str, trueLength) {
+  var output = '';
 
+  for (let i = 0; i < trueLength; i++) {
+    if (str.charAt(i) === ' ') {
+      output += '%20';
+    }
+    else {
+      output += str.charAt(i);
+    }
+  }
+
+  return output;
 }
 
 describe('1.3 URLifiy | Cracking the Coding Interview | Chapter 1 - Arrays and Strings', function() {
   describe('Input: "Mr John Smith     ", 13', function() {
     it('should return "Mr%20John%20Smith"', done => {
       expect(replaceSpaces('Mr John Smith     ', 13)).to.equal("Mr%20John%20Smith");
+      done();
+    })
+  })
+
+  describe('Input: "Coding is sexy     ", 14', function() {
+    it('should return "Coding%20is%20sexy"', done => {
+      expect(replaceSpaces('Coding is sexy     ', 14)).to.equal("Coding%20is%20sexy");
       done();
     })
   })
