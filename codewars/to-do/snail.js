77e035fcf57375c0f15c509467579ab868826fd0
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-24 08:25:08 -0400

    restarted snail & tests rewritten | 4kyu

--- codewars/to-do/snail.js
+++ codewars/to-do/snail.js
@@ -22,43 +22,59 @@ snail(array) #=> [1,2,3,4,5,6,7,8,9]
 */
 
 function snail(array) {
-  var width = array.length;
-  // console.log(width);
-  var height = array[0].length;
-  // console.log(height);
   var output = [];
-
-  for (let j = 0; j < array[0].length; j++) {
-    output.push(array[0][j]);
-    array[0][j] = '';
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    output.push(array[i][array[0].length - 1]);
-    array[i][array[0].length - 1] = '';
-  }
-
-  for (let j = array[array.length - 1].length; j > 0; j--) {
-    output.push(array[array.length - 1][j]);
-    array[array.length - 1][j] = '';
-  }
-
-  console.log(array);
-  return output;
+  var completedMatrix = [];
+  var temp = [];
 }
 
+var matrix1 = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+
+var matrix2 = [
+  [1,2,3],
+  [8,9,4],
+  [7,6,5]
+];
+
 describe('Snail | Code Wars | 4kyu', function() {
-  describe('Input: [[1,2,3], [4,5,6], [7,8,9]]', function() {
+  describe('Input: matrix1', function() {
     it('should return [1,2,3,6,9,8,7,4,5]', done => {
-      expect(snail([[1,2,3], [4,5,6], [7,8,9]])).to.deep.equal([1,2,3,6,9,8,7,4,5]);
+      expect(snail(matrix1)).to.deep.equal([1,2,3,6,9,8,7,4,5]);
       done();
     })
   })
 
-  describe('Input: [[1,2,3], [8,9,4], [7,6,5]]', function() {
+  describe('Input: matrix2', function() {
     it('should return [1,2,3,4,5,6,7,8,9]', done => {
-      expect(snail([[1,2,3], [8,9,4], [7,6,5]])).to.deep.equal([1,2,3,4,5,6,7,8,9]);
+      expect(snail(matrix2)).to.deep.equal([1,2,3,4,5,6,7,8,9]);
       done();
     })
   })
 })
+
+// var width = array.length;
+// // console.log(width);
+// var height = array[0].length;
+// // console.log(height);
+// var output = [];
+//
+// for (let j = 0; j < array[0].length; j++) {
+//   output.push(array[0][j]);
+//   array[0][j] = '';
+// }
+//
+// for (let i = 0; i < array.length; i++) {
+//   output.push(array[i][array[0].length - 1]);
+//   array[i][array[0].length - 1] = '';
+// }
+//
+// for (let j = array[array.length - 1].length; j > 0; j--) {
+//   output.push(array[array.length - 1][j]);
+//   array[array.length - 1][j] = '';
+// }
+//
+// console.log(array);
+// return output;
