4648d6017cb489383978ca6765df0161a65a703e
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-24 10:53:00 -0400

    function written for 2.4 - Parition | Cracking the Coding Interview | Chapter 2 - Linked Lists

--- cracking-the-coding-interview/2-linked-lists/2.4/2.4-partition.js
+++ cracking-the-coding-interview/2-linked-lists/2.4/2.4-partition.js
@@ -6,7 +6,24 @@
 Write code to partition a linked list aorund a value x, such that all nodes less than x come before all nodes greater than or equal to x. If x is contained within the list, the values of x only need ot be after the elements less than x (see below). The partition element x can appear anywhere in the "right partition"; it does not need to appear between the left and right partitions.
 
 EXAMPLE
-Input: 3 -> 5 -> 8 -> -> 10 -> 2 -> 1
+Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
 [partition = 5]
 Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 */
+
+function partition(linkedList, partition) {
+  var output = [];
+  var bigBoys = [];
+  var currentNode = linkedList.head;
+
+  while (currentNode.next) {
+    if (currentNode.value > partition) {
+      output.push(currentNode);
+    }
+    else {
+      bigBoys.push(currentNode);
+    }
+  }
+
+  return output.join(bigBoys);
+}
