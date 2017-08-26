f16feede6f3220ff90f910fec80dc1066302b069
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-26 12:15:20 -0700

    progress on 10.1 - Sorted Merge

--- cracking-the-coding-interview/10-sorting-and-searching/10.1-sorted-merge.js
+++ cracking-the-coding-interview/10-sorting-and-searching/10.1-sorted-merge.js
@@ -9,13 +9,31 @@ You are given two sorted arrays, A and B, where A has a large enough buffer at t
 */
 
 function sortedMerge(A, B) {
+  var pointerB = B.length - 1;
+  var pointerA = A.length - 1;
 
+  while (pointerB >= 0) {
+    if (B[pointerB] < A[pointerA]) {
+      while (A[pointerA] > B[pointerB]) {
+        pointerA--;
+      }
+      A.splice(pointerA, 0, B[pointerB])
+      A.join();
+      pointerB--;
+    }
+    else {
+      A.push(pointerB);
+      pointerB--;
+    }
+  }
+
+  console.log(A);
 }
 
 describe('10.1 - Sorted Merge | Cracking the Coding Interview | Chapter 10 - Sorting and Searching', function() {
-  describe('Input: [1, 5, 9, 10, 11, 22, 27], [4, 8, 10, 15, 17]', function() {
-    it('should return [1, 4, 5, 8, 9, 10, 10, 11, 15, 17, 22, 27]', done => {
-      expect(sortedMerge([1, 5, 9, 10, 11, 22, 27], [4, 8, 10, 15, 17])).to.deep.equal([1, 4, 5, 8, 9, 10, 10, 11, 15, 17, 22, 27]);
+  describe('Input: [1, 5, 9, 10, 11, 22, 27], [4, 8, 15, 17]', function() {
+    it('should return [1, 4, 5, 8, 9, 10, 11, 15, 17, 22, 27]', done => {
+      expect(sortedMerge([1, 5, 9, 10, 11, 22, 27], [4, 8, 15, 17])).to.deep.equal([1, 4, 5, 8, 9, 10, 11, 15, 17, 22, 27]);
       done();
     })
   })
