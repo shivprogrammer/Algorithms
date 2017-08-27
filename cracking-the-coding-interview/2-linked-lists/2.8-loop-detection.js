2fd7336c00a999816cd2baad85c72f0e5b89d00b
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-26 17:42:40 -0700

    I WOKE UP IN BEAST MODEEEEEEE TO SOLVE ALGORITHMS

--- cracking-the-coding-interview/2-linked-lists/2.8-loop-detection.js
+++ cracking-the-coding-interview/2-linked-lists/2.8-loop-detection.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 2.8 Loop Detection
 
@@ -13,6 +15,49 @@ Input: A -> B -> C -> D -> E -> C [the same C as earlier]
 Output: C
 */
 
-function loopDetection(linkedList) {
-
+function LinkedList(node) {
+  this.head = node;
 }
+
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+function loopDetection(linkedList) {
+  var nextValues = new Map();
+  var currentNode = linkedList.head;
+
+  while (currentNode) {
+    if (!nextValues.has(currentNode.next)) {
+      nextValues.set(currentNode.next, currentNode);
+      currentNode = currentNode.next;
+    }
+    else {
+      return currentNode.next;
+    }
+  }
+}
+
+var A = new Node('A');
+var B = new Node('B');
+var C = new Node('C');
+var D = new Node('D');
+var E = new Node('E');
+
+A.next = B;
+B.next = C;
+C.next = D;
+D.next = E;
+E.next = C;
+
+var sexyList = new LinkedList(A);
+
+describe('2.8 - Loop Detection | Cracking the Coding Interview | Chapter 2 - Linked Lists', function() {
+  describe('Input: sexyList', function() {
+    it('should return C', done => {
+      expect(loopDetection(sexyList)).to.equal(C);
+      done();
+    })
+  })
+})
