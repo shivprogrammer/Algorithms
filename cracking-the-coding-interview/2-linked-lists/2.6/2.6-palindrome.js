cac951247121db3d31c8279764511955d68f1d0a
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-26 18:55:20 -0700

    added LinkedList and Node constructors to 2.6 - Palindrome

--- cracking-the-coding-interview/2-linked-lists/2.6/2.6-palindrome.js
+++ cracking-the-coding-interview/2-linked-lists/2.6/2.6-palindrome.js
@@ -6,6 +6,19 @@
 Implement a function to check if a linked list is a palindrome
 */
 
-function palindrome(linkedList) {
-  
+function LinkedList(node) {
+  this.head = node;
 }
+
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+function palindrome(linkedList) {
+
+}
+
+describe('2.6 - Palindrome | Cracking the Coding Interview | Chapter 2 - Linked Lists', function() {
+  describe('Input:')
+})
