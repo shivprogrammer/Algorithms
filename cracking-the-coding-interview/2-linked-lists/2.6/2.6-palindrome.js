b5000e6f924bbde40c2c7d73a80d55fd999e2602
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-26 19:25:07 -0700

    added another test and time and space complexity | 2.6 - Palindrome

--- cracking-the-coding-interview/2-linked-lists/2.6/2.6-palindrome.js
+++ cracking-the-coding-interview/2-linked-lists/2.6/2.6-palindrome.js
@@ -40,23 +40,42 @@ function palindrome(linkedList) {
   return true;
 }
 
+// Time Complexity: O(N) [length of the LinkedList]
+// Space Complexity: O(N) [output] + O(X) [left]
+
 var node1 = new Node('A');
 var node2 = new Node('B');
 var node3 = new Node('C');
 var node4 = new Node('B');
 var node5 = new Node('A');
-
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node5;
-
 var awesomeList = new LinkedList(node1);
+
+var noder1 = new Node('A');
+var noder2 = new Node('B');
+var noder3 = new Node('C');
+var noder4 = new Node('C');
+var noder5 = new Node('A');
+noder1.next = noder2;
+noder2.next = noder3;
+noder3.next = noder4;
+noder4.next = noder5;
+var noderList = new LinkedList(noder1);
 
 describe('2.6 - Palindrome | Cracking the Coding Interview | Chapter 2 - Linked Lists', function() {
   describe('Input: awesomeList', function() {
     it('should return true', done => {
       expect(palindrome(awesomeList)).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: noderList', function() {
+    it('should return false', done => {
+      expect(palindrome(noderList)).to.equal(false);
       done();
     })
   })
