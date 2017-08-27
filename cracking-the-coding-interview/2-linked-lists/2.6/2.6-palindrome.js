9efb1e574fc78f91e6f023458a87e67031b24787
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-26 19:19:00 -0700

    test passing for 2.6 - Palindromegit add . :)

--- cracking-the-coding-interview/2-linked-lists/2.6/2.6-palindrome.js
+++ cracking-the-coding-interview/2-linked-lists/2.6/2.6-palindrome.js
@@ -20,26 +20,38 @@ function Node(value) {
 function palindrome(linkedList) {
   var listLength = 0;
   var currentNode = linkedList.head;
+  var output = [];
 
   while (currentNode) {
     listLength++;
+    output.push(currentNode.value);
     currentNode = currentNode.next;
   }
-  listLength++;
 
-  return listLength;
+  var mid = Math.ceil(listLength / 2);
+  var left = output.splice(0, mid);
+
+  for (let i = 0; i < output.length; i++) {
+    if (left[i] != output[output.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-var A = new Node('A');
-var B = new Node('B');
-var C = new Node('C');
+var node1 = new Node('A');
+var node2 = new Node('B');
+var node3 = new Node('C');
+var node4 = new Node('B');
+var node5 = new Node('A');
 
-A.next = B;
-B.next = C;
-C.next = B;
-B.next = A;
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
-var awesomeList = new LinkedList(A);
+var awesomeList = new LinkedList(node1);
 
 describe('2.6 - Palindrome | Cracking the Coding Interview | Chapter 2 - Linked Lists', function() {
   describe('Input: awesomeList', function() {
