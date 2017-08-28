6241ed59abaa1c714a74f1f6c09adc3e5604f49d
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-28 16:03:06 -0700

    Graph and Node constructors added for 4.1 - Route Between Nodes

--- cracking-the-coding-interview/4-trees-and-graphs/4.1/4.1-route-between-nodes.js
+++ cracking-the-coding-interview/4-trees-and-graphs/4.1/4.1-route-between-nodes.js
@@ -6,6 +6,15 @@
 Given a directed graph, design an algorithm to find out whether there is a route between two nodes
 */
 
+function Graph(node) {
+  this.root = node;
+}
+
+function Node(name) {
+  this.name = name;
+  this.children = [];
+}
+
 function routeBetweenNodes(graph) {
-  
+
 }
