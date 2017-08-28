60a83ce5fe4710761c9280e1e4c9b0facb469455
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-28 02:03:24 -0700

    constructors LinkedList and Node added | 2.7 - Intersection

--- cracking-the-coding-interview/2-linked-lists/2.7/2.7-intersection.js
+++ cracking-the-coding-interview/2-linked-lists/2.7/2.7-intersection.js
@@ -6,6 +6,15 @@
 Given two (singly) linked lists, determine if the two lists intersect. Return the intersecting node. Note that the intersection is defined based on reference, not value. That is, if the kth node of the first linked list is the exact same node (by reference) as the jth node of the second linked list, then they are intersecting.
 */
 
+function LinkedList(node) {
+  this.head = node;
+}
+
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
 function intersection(linkedList1, linkedList2) {
-  
+
 }
