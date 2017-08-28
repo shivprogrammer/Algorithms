0431a7115e1d40223c9ae198916c3bb2bf847454
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-27 21:43:09 -0700

    started 1.5 - One Away | Cracking the Coding Interview | Chapter 1 - Arrays and Strings

---
+++ cracking-the-coding-interview/1-arrays-and-strings.js/1.5/1.5-one-away.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+1.5 One Away
+
+There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away
+
+EXAMPLE:
+pale, ple -> true
+pales, pale -> true
+pale, bale -> true
+pale, bake -> false
+*/
+
+function oneAway(str1, str2) {
+
+}
