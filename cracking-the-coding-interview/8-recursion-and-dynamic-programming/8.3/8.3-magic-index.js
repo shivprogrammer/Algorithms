b9d3c4b6b1531e6d9568cafd8618abdf16e6ec12
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-30 13:35:37 -0700

    fixing bug in magicIndexRecursion that fails test when magic index is last element

--- cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
+++ cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
@@ -8,38 +8,65 @@ const expect = require('chai').expect;
 A magic index in an array[0, ..., n - 1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 */
 
-function magicFast(array) {
-  return magicFast(array, 0, array.length - 1);
+function magicIndex(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === i) {
+       return i;
+    }
+  }
+  return -1;
 }
 
-function magicFast(array, start, end) {
-  if (end < start) {
+function magicIndexRecursion(array, start, end) {
+  if (!start) {
+    start = 0;
+  }
+  if (!end) {
+    end = array.length - 1;
+  }
+  
+  var mid = Math.floor(start + (end - start) / 2);
+
+  if (mid === start && array[mid] !== mid) {
     return -1;
   }
-
-  var mid = (start + end) / 2;
-  if (array[mid] === mid) {
+  else if (array[mid] === mid) {
     return mid;
   }
-  else if (array[mid] > mid) {
-    return magicFast(array, start, mid - 1);
+  else if (mid < array[mid]) {
+    return magicIndexRecursion(array, start, mid);
   }
-  else  {
-    return magicFast(array, mid + 1, end);
+  else {
+    return magicIndexRecursion(array, mid, end);
   }
 }
 
 describe('8.3 - Magic Index | Cracking the Coding Interview | Chapter 8 - Recursion and Dynamic Programming', function() {
-  describe('Input: [-10, 2, 2, 8]', function() {
-    it('should return 2', done => {
-      expect(magicFast([-10, 2, 2, 8], -10, 8)).to.equal(2);
+  describe('Function: magicIndex | Magic Index is last element: [-1, 0, 1, 2, 3, 5]', function() {
+    it('should return 5', done => {
+      expect(magicIndex([-1, 0, 1, 2, 3, 5])).to.equal(5);
       done();
     })
   })
 
-  describe('Input: [-10, 2, 17, 22, 28]', function() {
+// when the magic index is the last int, the test fails for recursion
+  describe('Function: magicIndexRecursion | Magic Index is last element: [-1, 0, 1, 2, 3, 5]', function() {
+    it('should return 5', done => {
+      expect(magicIndexRecursion([-1, 0, 1, 2, 3, 5])).to.equal(5);
+      done();
+    })
+  })
+
+  describe('Function: magicIndex | No magic index in this input: [-10, 2, 17, 22, 28]', function() {
     it('should return -1', done => {
-      expect(magicFast([-10, 2, 17, 22, 28], -10, 28)).to.equal(-1);
+      expect(magicIndex([-10, 2, 17, 22, 28])).to.equal(-1);
+      done();
+    })
+  })
+
+  describe('Function: magicIndexRecursion | No magic index in this input: [-10, 2, 17, 22, 28]', function() {
+    it('should return -1', done => {
+      expect(magicIndexRecursion([-10, 2, 17, 22, 28])).to.equal(-1);
       done();
     })
   })
