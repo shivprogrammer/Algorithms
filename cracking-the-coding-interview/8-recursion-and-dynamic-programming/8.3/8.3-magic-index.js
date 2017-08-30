e79531b4671de2400e8dfc16819221a8ac6e9f03
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-30 00:34:11 -0700

    brute force solution's time and space complexity added | 8.3 - Magic Index

--- cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
+++ cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
@@ -14,12 +14,24 @@ function magicIndex(array) {
       return i;
     }
   }
+
+  return 'no magic index';
 }
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
 
 describe('8.3 - Magic Index | Cracking the Coding Interview | Chapter 8 - Recursion and Dynamic Programming', function() {
   describe('Input: [-10, 2, 2, 8]', function() {
     it('should return 2', done => {
       expect(magicIndex([-10, 2, 2, 8])).to.equal(2);
+      done();
+    })
+  })
+
+  describe('Input: [-10, 2, 17, 22, 28]', function() {
+    it('should return "no magic index"', done => {
+      expect(magicIndex([-10, 2, 17, 22, 28])).to.equal('no magic index');
       done();
     })
   })
