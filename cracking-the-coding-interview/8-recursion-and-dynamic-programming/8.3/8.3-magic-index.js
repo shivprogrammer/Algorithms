5e33f08ede5cca1b803147c9ba354bcfd1aa9970
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-29 23:58:08 -0700

    test added for 8.3 - Magic Index

--- cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
+++ cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 8.3 - Magic Index
 
@@ -9,3 +11,12 @@ A magic index in an array[0, ..., n - 1] is defined to be an index such that A[i
 function magicIndex(array) {
 
 }
+
+describe('8.3 - Magic Index | Cracking the Coding Interview | Chapter 8 - Recursion and Dynamic Programming', function() {
+  describe('Input: [-10, 2, 2, 8]', function() {
+    it('should return 2', done => {
+      expect(magicIndex([-10, 2, 2, 8])).to.equal(2);
+      done();
+    })
+  })
+})
