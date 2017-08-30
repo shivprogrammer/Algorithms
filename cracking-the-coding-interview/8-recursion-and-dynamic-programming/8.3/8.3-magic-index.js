52ba4016d9df3fd244ec65d041aeb7e58fc978ed
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-29 23:56:01 -0700

    started 8.3 - Magic Index | Cracking the Coding Interview | Chapter 8 - Recursion and Dynamic Programming

---
+++ cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+8.3 - Magic Index
+
+A magic index in an array[0, ..., n - 1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+*/
+
+function magicIndex(array) {
+
+}
