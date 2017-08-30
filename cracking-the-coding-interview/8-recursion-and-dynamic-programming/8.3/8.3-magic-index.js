7084e75950aa666fdfd740eecc7aa22ca34ef941
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-30 13:04:39 -0700

    refactoring 8.3 - Magic Index using recursion for speed, 1/2 tests passing

--- cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
+++ cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.3/8.3-magic-index.js
@@ -8,30 +8,38 @@ const expect = require('chai').expect;
 A magic index in an array[0, ..., n - 1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 */
 
-function magicIndex(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === i) {
-      return i;
-    }
-  }
-
-  return 'no magic index';
+function magicFast(array) {
+  return magicFast(array, 0, array.length - 1);
 }
 
-// Time Complexity: O(N)
-// Space Complexity: O(1)
+function magicFast(array, start, end) {
+  if (end < start) {
+    return -1;
+  }
+
+  var mid = (start + end) / 2;
+  if (array[mid] === mid) {
+    return mid;
+  }
+  else if (array[mid] > mid) {
+    return magicFast(array, start, mid - 1);
+  }
+  else  {
+    return magicFast(array, mid + 1, end);
+  }
+}
 
 describe('8.3 - Magic Index | Cracking the Coding Interview | Chapter 8 - Recursion and Dynamic Programming', function() {
   describe('Input: [-10, 2, 2, 8]', function() {
     it('should return 2', done => {
-      expect(magicIndex([-10, 2, 2, 8])).to.equal(2);
+      expect(magicFast([-10, 2, 2, 8], -10, 8)).to.equal(2);
       done();
     })
   })
 
   describe('Input: [-10, 2, 17, 22, 28]', function() {
-    it('should return "no magic index"', done => {
-      expect(magicIndex([-10, 2, 17, 22, 28])).to.equal('no magic index');
+    it('should return -1', done => {
+      expect(magicFast([-10, 2, 17, 22, 28], -10, 28)).to.equal(-1);
       done();
     })
   })
