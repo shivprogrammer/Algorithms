ab34462060d49b40468657aba3123966e803d3e5
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-30 19:17:45 -0700

    started 8.11 - Coins | Cracking the Coding Interview | Chapter 8 - Recursion & Dynamic Programming

---
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.11/8.11-coins.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+8.1 - Coins
+
+Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent), write code to calculate the number of ways representing n cents.
+*/
+
+function coins(n) {
+  
+}
