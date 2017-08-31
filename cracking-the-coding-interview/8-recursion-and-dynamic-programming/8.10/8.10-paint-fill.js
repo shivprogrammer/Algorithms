7a92bdf76fbdd61dd43eeb8ffdf7c704a723ccab
shivprogrammer <shiv.unadkat@gmail.com>
2017-08-30 18:46:05 -0700

    started 8.10 - Paint Fill | Cracking the Coding Interview | Chapter 8 - Recursion & Dynamic Programming

---
+++ cracking-the-coding-interview/8-recursion-and-dynamic-programming/8.10/8.10-paint-fill.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+8.10 - Paint Fill
+
+Implement the "paint fill" function that one might see on many image editing programs. That is, given a screen (represented by a two-dimensional array of colors), a point, and a new color, fill in the surrounding area until the color changes from the original color.
+*/
+
+function paintFill(screen, row, col, color) {
+
+}
