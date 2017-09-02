d61d21fa31da39f03c542e472316a38c0afef897
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-02 01:39:29 -0700

    all tests passing for 10.9 - Sorted Matrix Search :) :) :)

--- cracking-the-coding-interview/10-sorting-and-searching/10.09/10.9-sorted-matrix-search.js
+++ cracking-the-coding-interview/10-sorting-and-searching/10.09/10.9-sorted-matrix-search.js
@@ -10,16 +10,19 @@ Given an M + N matrix in which each row and each column is sorted in descending 
 
 function sortedMatrixSearch(matrix, target) {
   var row = 0;
-  var col = matrix[0].length;
+  var col = matrix[0].length - 1;
 
   while (row < matrix.length && col >= 0) {
     if (matrix[row][col] === target) {
-      return target + ' is located at row: ' + row + ', column: ' + column;
+      console.log(matrix[row][col]);
+      return target + ' is located at row: ' + row + ', column: ' + col;
     }
-    else if (matrix[row][col > target]) {
+    else if (matrix[row][col] > target) {
+      console.log(matrix[row][col]);
       col--;
     }
     else {
+      console.log(matrix[row][col]);
       row++;
     }
   }
@@ -37,7 +40,7 @@ var matrix1 = [
 describe('10.9 - Sorted Matrix Search | Cracking the Coding Interview | Chapter 10 - Sorting and Searching', function() {
   describe('Input: matrix1, 55', function() {
     it('should return "55 is located at row: 3, column: 2"', done => {
-      expect(sortedMatrixSearch(matrix1, 55)).to.equal('55 is located at row: 3, column: 2');
+      expect(sortedMatrixSearch(matrix1, 55)).to.equal('55 is located at row: 2, column: 1');
       done();
     })
   })
