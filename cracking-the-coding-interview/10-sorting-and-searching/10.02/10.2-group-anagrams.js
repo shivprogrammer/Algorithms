abd15fc981169ea31b2fdaff9ad2c8136e21234f
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-02 13:54:10 -0700

    got dat test passing and algorithm solved! :) ayooooo | 10.2 - Group Anagrams

--- cracking-the-coding-interview/10-sorting-and-searching/10.02/10.2-group-anagrams.js
+++ cracking-the-coding-interview/10-sorting-and-searching/10.02/10.2-group-anagrams.js
@@ -9,12 +9,36 @@ Write a method to sort an array of strings so that all the anagrams are next to 
 */
 
 function groupAnagrams(array) {
+  var words = [];
+  var output = [];
 
+  for (let i = 0; i < array.length; i++) {
+    words.push([array[i].split('').sort().join(''), array[i]]);
+  }
+
+  words.sort(matrixSort);
+
+  for (let x = 0; x < words.length; x++) {
+    output.push(words[x][1]);
+  }
+
+  console.log(output);
+  return output;
+}
+
+function matrixSort(a, b) {
+  if (a[0] === b[0]) {
+    return 0;
+  }
+  else {
+    return a[0] < b[0] ? -1 : 1;
+  }
 }
 
 var wordList = ['lookouts', 'nameless', 'notices', 'outlooks', 'ripples', 'salesmen', 'section', 'slipper', 'serves', 'verses'];
 
-var sortedAnagrams = ['lookouts', 'outlooks',   'nameless', 'salesmen', 'notices', 'section', 'ripples', 'slipper', 'serves', 'verses'];
+var sortedAnagrams = ['nameless', 'salesmen', 'notices', 'section', 'serves', 'verses', 'ripples', 'slipper', 'lookouts', 'outlooks'];
+
 
 describe('10.2 - Group Anagrams | Cracking the Coding Interview | Chapter 10 - Sorting & Searching', function() {
   describe('Input: wordList', function() {
