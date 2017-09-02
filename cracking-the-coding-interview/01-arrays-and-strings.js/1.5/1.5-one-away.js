910f43cf2f25c16766e626c77d7e814874724d90
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-02 14:32:57 -0700

    tests written for 1.5 - One Away

--- cracking-the-coding-interview/01-arrays-and-strings.js/1.5/1.5-one-away.js
+++ cracking-the-coding-interview/01-arrays-and-strings.js/1.5/1.5-one-away.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
-1.5 One Away
+1.5 - One Away
 
 There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away
 
@@ -15,3 +17,33 @@ pale, bake -> false
 function oneAway(str1, str2) {
 
 }
+
+describe('1.5 - One Away | Cracking the Coding Interview | Chapter 1 - Arrays & Strings', function() {
+  describe('Input: pale, ple', function() {
+    it('should return true', done => {
+      expect(oneAway('pale', 'ple')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: pales, pale', function() {
+    it('should return true', done => {
+      expect(oneAway('pales', 'pale')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: pale, bale', function() {
+    it('should return true', done => {
+      expect(oneAway('pale', 'bale')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('Input: pale, bake', function() {
+    it('should return false', done => {
+      expect(oneAway('pale', 'bake')).to.equal(false);
+      done();
+    })
+  })
+})
