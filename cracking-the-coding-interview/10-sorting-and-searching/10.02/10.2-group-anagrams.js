0b77fd15937213ab4e40eae0400ffb99f0281c7a
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-02 12:21:16 -0700

    test added for 10.2 - Group Anagrams

--- cracking-the-coding-interview/10-sorting-and-searching/10.02/10.2-group-anagrams.js
+++ cracking-the-coding-interview/10-sorting-and-searching/10.02/10.2-group-anagrams.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 10.02 - Group Anagrams
 
@@ -7,5 +9,18 @@ Write a method to sort an array of strings so that all the anagrams are next to 
 */
 
 function groupAnagrams(array) {
-  
+
 }
+
+var wordList = ['lookouts', 'nameless', 'notices', 'outlooks', 'ripples', 'salesmen', 'section', 'slipper', 'serves', 'verses'];
+
+var sortedAnagrams = ['lookouts', 'outlooks',   'nameless', 'salesmen', 'notices', 'section', 'ripples', 'slipper', 'serves', 'verses'];
+
+describe('10.2 - Group Anagrams | Cracking the Coding Interview | Chapter 10 - Sorting & Searching', function() {
+  describe('Input: wordList', function() {
+    it('should return sortedAnagrams', done => {
+      expect(groupAnagrams(wordList)).to.deep.equal(sortedAnagrams);
+      done();
+    })
+  })
+})
