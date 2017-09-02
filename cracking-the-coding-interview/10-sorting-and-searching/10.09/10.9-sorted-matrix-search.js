63abc32aef0eeecd2e93742a6b5a0a536936dafc
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-01 19:31:40 -0700

    started 10.9 - Sorted Matrix Search | Cracking thed Coding Interview | Chapter 9 - Sorting and Searching

---
+++ cracking-the-coding-interview/10-sorting-and-searching/10.09/10.9-sorted-matrix-search.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+10.9 - Sorted Matrix Search
+
+Given an M + N matrix in which each row and each column is sorted in descending order, write a method to find an element.
+*/
+
+function sortedMatrixSearch(matrix) {
+  
+}
