5ff22eca9d47ff08c2f575223c83202a1a25e03d
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-04 13:39:40 -0700

    test added for 1.9 - String Rotation

--- cracking-the-coding-interview/01-arrays-and-strings.js/1.9/1.9-string-rotation.js
+++ cracking-the-coding-interview/01-arrays-and-strings.js/1.9/1.9-string-rotation.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 1.9 - String Rotation
 
 Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g. "waterbottle" is a rotation of "erbottlewat").
 */
 
-function isSubstring(s1, s2) {
-  
+function isRotation(s1, s2) {
+
 }
+
+describe('1.9 - String Rotation | Cracking the Coding Interview | Chapter 1 - Arrays and Strings', function() {
+  describe('s1 = "waterbottle", s2 = "erbottlewat"', function() {
+    it('should return true', done => {
+      expect(isRotation('waterbottle', 'erbottlewat')).to.equal(true);
+      done();
+    })
+  })
+})
