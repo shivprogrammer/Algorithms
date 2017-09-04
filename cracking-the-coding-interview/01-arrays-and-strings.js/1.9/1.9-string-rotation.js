f687592b5afa2631a275ffae688dae9a25b54539
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-04 13:44:30 -0700

    holy shit I got it right on the first try | 1.9 - String Rotation

--- cracking-the-coding-interview/01-arrays-and-strings.js/1.9/1.9-string-rotation.js
+++ cracking-the-coding-interview/01-arrays-and-strings.js/1.9/1.9-string-rotation.js
@@ -9,13 +9,24 @@ Assume you have a method isSubstring which checks if one word is a substring of 
 */
 
 function isRotation(s1, s2) {
-
+  if (s1.length === s2.length) {
+    var longS1 = s1.concat(s1);
+    return longS1.includes(s2);
+  }
+  return false;
 }
 
 describe('1.9 - String Rotation | Cracking the Coding Interview | Chapter 1 - Arrays and Strings', function() {
   describe('s1 = "waterbottle", s2 = "erbottlewat"', function() {
     it('should return true', done => {
       expect(isRotation('waterbottle', 'erbottlewat')).to.equal(true);
+      done();
+    })
+  })
+
+  describe('s1 = "waterbottle", s2 = "rebottlewat"', function() {
+    it('should return false', done => {
+      expect(isRotation('waterbottle', 'rebottlewat')).to.equal(false);
       done();
     })
   })
