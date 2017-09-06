d42d564701efd5f09a4855141650864695570984
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-05 23:36:23 -0700

    10.3 - Search in Rotated Array test added

--- cracking-the-coding-interview/10-sorting-and-searching/10.03/10.03-search-in-rotated-array.js
+++ cracking-the-coding-interview/10-sorting-and-searching/10.03/10.03-search-in-rotated-array.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
 10.3 - Search in Rotated Array
 
@@ -9,3 +11,12 @@ Given a sorted array of n integers that has been rotated an unknown number of ti
 function rotatedArraySearch(array, element) {
 
 }
+
+describe('10.3 - Search in Rotated Array | Cracking the Coding Interview | Chapter 10 - Searching and Sorting', function() {
+  describe('array = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], element = 5', function() {
+    it('should return 8', done => {
+      expect(rotatedArraySearch([15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], 5)).to.equal(8);
+      done();
+    })
+  })
+})
