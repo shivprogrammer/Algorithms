1eedf5742e98eebb47019854c6fafb0e18a6dbbe
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-06 18:26:14 -0700

    all tests passing for all unique

--- codewars/working-on/all-unique.js
+++ codewars/working-on/all-unique.js
@@ -13,27 +13,37 @@ The string may contain any of the 128 ASCII characters.
 */
 
 function hasUniqueChars(str) {
+  var uniqueMap = new Map();
 
+  for (let i = 0; i < str.length; i++) {
+    if (uniqueMap.has(str.charAt(i))) {
+      return false;
+    }
+    else {
+      uniqueMap.set(str.charAt(i), 1);
+    }
+  }
+  return true;
 }
 
 describe('All Unique | Code Wars | 7kyu', function() {
   describe('Input: "  nAa"', function() {
     it('should return false', done => {
-      expect(isUnique('  nAa')).to.equal(false);
+      expect(hasUniqueChars('  nAa')).to.equal(false);
       done();
     })
   })
 
   describe('Input: "abcdef"', function() {
     it('should return true', done => {
-      expect(isUnique('abcdef')).to.equal(true);
+      expect(hasUniqueChars('abcdef')).to.equal(true);
       done();
     })
   })
 
   describe('Input: "++-"', function() {
     it('should return false', done => {
-      expect(isUnique('++-')).to.equal(false);
+      expect(hasUniqueChars('++-')).to.equal(false);
       done();
     })
   })
