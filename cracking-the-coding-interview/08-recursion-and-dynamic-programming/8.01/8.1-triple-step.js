03d5e8d0ecc0073618ee48fcc028eac0c12ad7ad
shivprogrammer <shiv.unadkat@gmail.com>
2017-09-07 21:26:20 -0700

    refactored 8.1 - Triple Steps, rewrote tests and added another test

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.01/8.1-triple-step.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.01/8.1-triple-step.js
@@ -8,6 +8,17 @@ const expect = require('chai').expect;
 A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the child can run up the stairs.
 */
 
+function tripleStep(steps) {
+  if (steps < 0) {
+    return 0;
+  }
+  else if (steps === 1) {
+    return 1;
+  }
+  else {
+    return tripleStep(steps - 1) + tripleStep(steps - 2) + tripleStep(steps - 3);
+  }
+}
 
 // Time Complexity: O(3 ^ N)
 // Space Complexity: O(1)
@@ -21,15 +32,22 @@ describe('8.1 - Triple Step | Cracking the Coding Interview | Chapter 8 - Recusi
   })
 
   describe('Function: tripleStep | Input: 3', function() {
-    it('should return 4', done => {
-      expect(tripleStep(3)).to.equal(4);
+    it('should return 2', done => {
+      expect(tripleStep(3)).to.equal(2);
       done();
     })
   })
 
   describe('Function: tripleStep | Input: 5', function() {
-    it('should return 15', done => {
-      expect(tripleStep(5)).to.equal(15);
+    it('should return 7', done => {
+      expect(tripleStep(5)).to.equal(7);
+      done();
+    })
+  })
+
+  describe('Function: tripleStep | Input: 6', function() {
+    it('should return 13', done => {
+      expect(tripleStep(6)).to.equal(13);
       done();
     })
   })
