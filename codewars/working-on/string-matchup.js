32122213b85bacb934612c8b553241cc60760695
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-01 23:54:40 -0700

    all tests passing, solved in 4 minutes

--- codewars/working-on/string-matchup.js
+++ codewars/working-on/string-matchup.js
@@ -22,26 +22,48 @@ Therefore, solve(array1, array2) = [2, 1, 0]
 */
 
 function solve(a,b) {
+  var output = [];
+  var amountMap = new Map();
+
+  for (let i = 0; i < a.length; i++) {
+    if (!amountMap.has(a[i])) {
+      amountMap.set(a[i], 1);
+    }
+    else {
+      amountMap.set(a[i], amountMap.get(a[i]) + 1);
+    }
+  }
+
+  for (let x = 0; x < b.length; x++) {
+    if (amountMap.has(b[x])) {
+      output.push(amountMap.get(b[x]));
+    }
+    else {
+      output.push(0);
+    }
+  }
+
+  return output;
 }
 
 describe('String Matchup | Code Wars | 7kyu', function() {
   describe('Input: ["abc", "abc","xyz","abcd","cde"], ["abc", "cde", "uap"]', function() {
     it('should return [2, 1, 0]', done => {
-      expect(solve(["abc", "abc","xyz","abcd","cde"], ["abc", "cde", "uap"])).to.equal([2, 1, 0]);
+      expect(solve(["abc", "abc","xyz","abcd","cde"], ["abc", "cde", "uap"])).to.deep.equal([2, 1, 0]);
       done();
     })
   })
 
   describe('Input: ["abc", "xyz","abc", "xyz","cde"], ["abc", "cde", "xyz"]', function() {
     it('should return [2, 1, 2]', done => {
-      expect(solve(['abc', 'xyz','abc', 'xyz','cde'], ['abc', 'cde', 'xyz'])).to.equal([2, 1, 2]);
+      expect(solve(['abc', 'xyz','abc', 'xyz','cde'], ['abc', 'cde', 'xyz'])).to.deep.equal([2, 1, 2]);
       done();
     })
   })
 
   describe('Input: ["quick", "brown", "fox", "is", "quick"], ["quick", "abc", "fox"]', function() {
     it('should return [2, 0, 1]', done => {
-      expect(solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox'])).to.equal([2, 0, 1]);
+      expect(solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox'])).to.deep.equal([2, 0, 1]);
       done();
     })
   })
