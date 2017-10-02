46721170ff181a8e8b72c35423638f07fdc941d1
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-01 23:47:33 -0700

    started string matchup | 7kyu

---
+++ codewars/working-on/string-matchup.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/*
+String Matchup
+
+7kyu
+
+Given two arrays of strings, return the number of times each string of the second array appears in the first array.
+
+Example
+
+array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
+array2 = ['abc', 'cde', 'uap']
+How many times do the elements in array2 appear in array1?
+
+'abc' appears twice in the first array (2)
+'cde' appears only once (1)
+'uap' does not appear in the first array (0)
+Therefore, solve(array1, array2) = [2, 1, 0]
+*/
+
+function solve(a,b) {
+}
+
+// Test.assertDeepEquals(solve(['abc', 'abc','xyz','abcd','cde'], ['abc', 'cde', 'uap']), [2, 1, 0]);
+// Test.assertDeepEquals(solve(['abc', 'xyz','abc', 'xyz','cde'], ['abc', 'cde', 'xyz']), [2, 1, 2]);
+// Test.assertDeepEquals(solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox']), [2, 0, 1]);
