f70b448bc5658bba1bf37e28863b504e0c8a95f4
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-02 19:04:04 -0700

    started move all vowels | 7kyu

---
+++ codewars/working-on/move-all-vowels.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+Given a string as input, move all of its vowel to the end of the string.
+
+Examples:
+input: "day" ==> output: "dya"
+input: "apple" ==> output: "pplae"
+
+Note: All input string are of small letters, and the order of the vowels at the end should be the same as they were before.
+*/
+
+function moveVowel(input) {
+
+}
