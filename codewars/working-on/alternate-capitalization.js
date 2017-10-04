72cbe3e8bd8e64610a87862e68268260a20d9529
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-03 19:59:29 -0700

    all tests passing for Alternate Capitalization

--- codewars/working-on/alternate-capitalization.js
+++ codewars/working-on/alternate-capitalization.js
@@ -15,34 +15,50 @@ The input will be a lowercase string with no spaces.
 */
 
 function capitalize(s) {
-  
+  var output = [];
+  var result1 = '';
+  var result2 = '';
+
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      result1 += s.charAt(i).toUpperCase();
+      result2 += s.charAt(i);
+    }
+    else {
+      result1 += s.charAt(i);
+      result2 += s.charAt(i).toUpperCase();
+    }
+  }
+
+  output.push(result1, result2);
+  return output;
 }
 
 describe('Alternate Capitalization | Code Wars | 7kyu', function() {
   describe('Input: "abcdef"', function() {
     it('should return ["AbCdEf", "aBcDeF"]', done => {
-      expect(capitalize('abcdef')).to.equal(['AbCdEf', 'aBcDeF']);
+      expect(capitalize('abcdef')).to.deep.equal(['AbCdEf', 'aBcDeF']);
       done();
     })
   })
 
   describe('Input: "codewars"', function() {
     it('should return ["CoDeWaRs", "cOdEwArS"]', done => {
-      expect(capitalize('codewars')).to.equal(['CoDeWaRs', 'cOdEwArS']);
+      expect(capitalize('codewars')).to.deep.equal(['CoDeWaRs', 'cOdEwArS']);
       done();
     })
   })
 
   describe('Input: "abracadabra"', function() {
     it('should return ["AbRaCaDaBrA", "aBrAcAdAbRa"]', done => {
-      expect(capitalize('abracadabra')).to.equal(['AbRaCaDaBrA', 'aBrAcAdAbRa']);
+      expect(capitalize('abracadabra')).to.deep.equal(['AbRaCaDaBrA', 'aBrAcAdAbRa']);
       done();
     })
   })
 
   describe('Input: "codewarriors"', function() {
     it('should return ["CoDeWaRrIoRs", "cOdEwArRiOrS"]', done => {
-      expect(capitalize('codewarriors')).to.equal(['CoDeWaRrIoRs', 'cOdEwArRiOrS']);
+      expect(capitalize('codewarriors')).to.deep.equal(['CoDeWaRrIoRs', 'cOdEwArRiOrS']);
       done();
     })
   })
