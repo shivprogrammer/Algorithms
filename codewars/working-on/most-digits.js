5c045194bbc627304088a819f4456040e5908c3d
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-07 14:46:18 -0700

    tests added for Most Digits | done in 3 min

--- codewars/working-on/most-digits.js
+++ codewars/working-on/most-digits.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const expect = require('chai').expect;
+
 /*
+Most Digits
+
+7kyu
+
 Find the number with the most digits.
 
 If two numbers in the argument array have the same number of digits, return the first one in the array.
@@ -8,3 +14,26 @@ If two numbers in the argument array have the same number of digits, return the 
 
 function findLongest(array) {
 }
+
+describe('Most Digits | Code Wars | 7kyu', function() {
+  describe('Input: [1, 10, 100]', function() {
+    it('should return 100', done => {
+      expect(findLongest([1, 10, 100])).to.equal(100);
+      done();
+    })
+  })
+
+  describe('Input: [9000, 8, 800]', function() {
+    it('should return 9000', done => {
+      expect(findLongest([9000, 8, 800])).to.equal(9000);
+      done();
+    })
+  })
+
+  describe('Input: [8, 900, 500]', function() {
+    it('should return 900', done => {
+      expect(findLongest([8, 900, 500])).to.equal(900);
+      done();
+    })
+  })
+})
