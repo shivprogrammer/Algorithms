0a4244ead574098f9dd1bcc4cad6641e1af8e22d
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-14 21:54:33 -0700

    started Heron's Formula | 7kyu

---
+++ codewars/working-on/heron-formula.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+Heron's Formula
+
+7kyu
+
+Write function heron which calculates the area of a triangle with sides a, b, and c.
+
+Heron's formula: sqrt (s * (s - a) * (s - b) * (s - c)), where s = (a + b + c) / 2
+*/
+
+function heron() {
+}
