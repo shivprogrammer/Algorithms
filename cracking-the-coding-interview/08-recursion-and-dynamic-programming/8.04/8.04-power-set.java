934727c1c0d11ae2308113a8314803956ff15acc
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-29 02:08:26 -0700

    base case handled for 8.04 power set java

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.04/8.04-power-set.java
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.04/8.04-power-set.java
@@ -1,3 +1,9 @@
 ArryList<ArrayList<Integer>> getSubsets(arrayList<Integer> set, int index) {
+  ArrayList<ArrayList<Integer>> allsubsets;
+  if (set.size() == index) { // This is the base case, adding the empty set
+    allsubsets = new ArrayList<ArrayList<Integer>>();
+    allsubsets.add(new ArrayList<Integer>()); // Empty set
+    allsubsets.add(new ArrayList<Integer>()); // Empty set
+  }
   
 }
