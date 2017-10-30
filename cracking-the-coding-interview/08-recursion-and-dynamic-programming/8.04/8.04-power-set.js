eacdaecbdf885beab64d5745dee0423255bd1950
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-29 18:23:29 -0700

    added handwritten picture of 8.4 Power Set in Javascript

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.04/8.04-power-set.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.04/8.04-power-set.js
@@ -8,13 +8,14 @@ Write a method to return all subsets of a set
 
 var returnSubsets = function(set) {
   var subsets = [];
-  var recurse = function(currset, remainingSet) {
+  var recurse = function(currSet, remainingSet) {
     subsets.push(currSet);
-    for (var i = 0; i < remainingSet.lenghth; i++) {
-      recurse(currSet.concat([remainingSet[i]], remainingSet.slice(i + 1)));
+    for (var i = 0; i < remainingSet.length; i++) {
+      recurse(currSet.concat([remainingSet[i]]), remainingSet.slice(i + 1));
     }
   }
   recurse([], set);
-  console.log(subsets);
   return subsets;
 }
+
+console.log(returnSubsets([1, 2, 3, 4]));
