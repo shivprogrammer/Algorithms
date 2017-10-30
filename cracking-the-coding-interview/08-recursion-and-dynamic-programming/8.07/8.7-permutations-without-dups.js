d61fe91c11143d1a44bb7c43d283d6d60a9ed362
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-30 13:02:59 -0700

    solution added for 8.7 - Permutation without Dups in Javascript

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.07/8.7-permutations-without-dups.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.07/8.7-permutations-without-dups.js
@@ -5,3 +5,20 @@
 
 Write a method to compute all permutations of a string of unique characters.
 */
+
+var permuteString = function(string) {
+  var answers = [];
+  var recurse = function(currPerm, remainingChars) {
+    if (remainingChars.length === 0) {
+      answers.push(currPerm);
+    }
+    else {
+      for (var i = 0; i < remainingChars.length; i++) {
+        recurse(currPerm + remainingChars.charAt(i), remainingChars.slice(0, i) + remainingChars.slice(i + 1));
+      }
+    }
+  }
+
+  recurse('', string);
+  return answers;
+}
