37cffe15b669e74522fcea0174be448e7a475e72
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-29 23:49:24 -0700

    added additional test to 8.4 - Power Set and its passinggit add .

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.04/8.4-power-set.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.04/8.4-power-set.js
@@ -11,6 +11,9 @@ Write a method to return all subsets of a set
 var returnSubsets = function(set) {
   var subsets = [];
   var recurse = function(currSet, remainingSet) {
+    console.log(subsets, 'subsets')
+    console.log(currSet, 'currSet')
+    console.log(remainingSet, 'remainingSet')
     subsets.push(currSet);
     for (var i = 0; i < remainingSet.length; i++) {
       recurse(currSet.concat([remainingSet[i]]), remainingSet.slice(i + 1));
@@ -20,12 +23,17 @@ var returnSubsets = function(set) {
   return subsets;
 }
 
-console.log(returnSubsets([1, 2, 3, 4]));
-
 describe('8.4 - Power Set | Cracking the Coding Interview | Chapter 8 - Recursion and Dynamic Programming', function() {
   describe('Input: [1, 2, 3, 4]', function() {
     it('should return [ [], [1], [1, 2], [1, 2, 3], [1, 2, 3, 4], [1, 2, 4], [1, 3], [1, 3, 4], [1, 4], [2], [2, 3], [2, 3, 4], [2, 4], [3], [3, 4], [4] ]', done => {
       expect(returnSubsets([1, 2, 3, 4])).to.deep.equal([ [], [1], [1, 2], [1, 2, 3], [1, 2, 3, 4], [1, 2, 4], [1, 3], [1, 3, 4], [1, 4], [2], [2, 3], [2, 3, 4], [2, 4], [3], [3, 4], [4] ]);
+      done();
+    })
+  })
+
+  describe('Input: [1, 2, 3]', function() {
+    it('should return [ [], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3] ]', done => {
+      expect(returnSubsets([1, 2, 3])).to.deep.equal([ [], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3] ]);
       done();
     })
   })
