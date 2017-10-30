97c1c35191f15e2238792051e80149cc4e21cc18
shivprogrammer <shiv.unadkat@gmail.com>
2017-10-30 12:51:54 -0700

    solution complete for 8.7 - Permutations without Dups in Java

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.07/8.7-permutations-without-dups.java
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.07/8.7-permutations-without-dups.java
@@ -1,3 +1,21 @@
 ArrayList<String> getPerms(String str) {
   if (str === null) return null;
+
+  ArrayList<String> permutations = new ArrayList<String>();
+  if (str.length() == 0) {
+    permutations.add(""); // adding the empty string for base case
+    return permutations
+  }
+
+  char first = str.charAt(0); // get the first character
+  String remainder = str.subString(1); // all the characters except for the first one
+  ArrayList<String> words = getPerms(remainder);
+  for (String word : words) {
+    for (int j= 0; i <= word.length(); j++) {
+      String s = insertCharAt(word, first, j);
+      permutations.add(s);
+    }
+  }
+
+  return permutations;
 }
