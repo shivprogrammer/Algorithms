f2ab06917f104c9d54dfdebf767dd4f21231690e
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-02 22:51:15 -0700

    solution completed for 8.8 - Permutations with Duplicates

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.08/8.8-permutations-with-duplicates.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.08/8.8-permutations-with-duplicates.js
@@ -6,3 +6,23 @@
 Write a method to compute all permutations of a string whose characters are not necessairily unique. The list of permutations should not have duplicates.
 
 */
+
+var permNoDups = function(string) {
+  var answers = [];
+  var recurse = function(currPerm, remainingChars) {
+    if (remainingChars.length === 0) {
+      answers.push(currPerm);
+    }
+    else {
+      var usedChars = {};
+      for (var i = 0; i < remainingChars.length; i++) {
+        if (!usedChars[remainingChars.charAt(i)]) {
+          usedChars[remainingChars.charAt(i)] = true;
+          recurse(currPerm + remainingChars.charAt(i), remainingChars.slice(0, i) + remainingChars.slice(i + 1));
+        }
+      }
+    }
+  }
+  recurse('', string);
+  return answers;
+}
