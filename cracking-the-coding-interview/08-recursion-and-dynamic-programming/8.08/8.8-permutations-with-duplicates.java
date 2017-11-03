2c61cd3ebcf8a46796c3f2ef59e21ae9457525bf
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-02 22:22:18 -0700

    added buildFreqTable to solution in Java | 8.8 - Permutations with DuplicateS

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.08/8.8-permutations-with-duplicates.java
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.08/8.8-permutations-with-duplicates.java
@@ -2,5 +2,16 @@ ArrayList<String> printPerms(String s) {
   ArrayList<String> result = new ArrayList<String>();
   HashMap<Character, Integer> map = buildFreqTable(s);
   printPerms(map, "", s.length(), result);
-  return reuslt;
+  return result;
+}
+
+HashMap<Character, Integer> buildFreqTable(String s) {
+  HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+  for (char c : toCharArray()) {
+    if (!map.containKey(c)) {
+      map.put(c, 0);
+    }
+    map.put(c, map.get(c) + 1);
+  }
+  return map;
 }
