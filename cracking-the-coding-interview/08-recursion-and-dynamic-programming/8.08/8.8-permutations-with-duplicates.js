a8e955cdff77d09b8dd000d5d9bc7878bfd0afd9
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-02 22:47:42 -0700

    started 8.8 - Permuations with Duplicates | Added instructions

---
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.08/8.8-permutations-with-duplicates.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/*
+8.8 - Permutations with Duplicates
+
+Write a method to compute all permutations of a string whose characters are not necessairily unique. The list of permutations should not have duplicates.
+
+*/
