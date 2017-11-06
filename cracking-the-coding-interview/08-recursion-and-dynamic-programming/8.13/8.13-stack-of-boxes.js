0a70ea207ee91d23d00b20c90372b471eedbe69d
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-06 10:28:28 -0800

    Started 8.13 - Stack of Boxes

---
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.13/8.13-stack-of-boxes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/*
+8.13 - Stack of Boxes
+
+You have a stack of n boxes, with widths w, heights h, and depths d. The boses cannot be rotated and can only be stacked on top of one another if each box in the stack is strictly larger than the box above it in width, height, and depth. Implement a method to compute the height of the tallest possible stack. The height of the stack is the sum of the heights of each box.
+*/
