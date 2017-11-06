1c30cba80023f8da7f5293dbc3f05a363809768e
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-06 11:03:18 -0800

    solution complete for 8.13 - Stack of Boxes

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.13/8.13-stack-of-boxes.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.13/8.13-stack-of-boxes.js
@@ -17,3 +17,25 @@ var findStackables = (boxes, basebox) => {
   })
   return stackables;
 }
+
+var stackBoxes = function(boxes) {
+  if (boxes === undefined) {
+    return 'where the boxes at doe?';
+  }
+
+  if (boxes.length === 0) {
+    return 0;
+  }
+  boxes.sort();
+  var key = JSON.stringify(boxes);
+  if (dp[key] === undefined) {
+    var height = 0;
+    boxes.forEach((box) => {
+      var stackables = findStackables(boxes, box);
+      var currHeight = box.height + stackBoxes(stackables);
+      height = Math.max(currHeight, height);
+    })
+    dp[key] = height;
+  }
+  return dp[key];
+}
