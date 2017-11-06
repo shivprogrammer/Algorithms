bda53805640f18ff605b422bd029c2e19e871367
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-05 17:54:37 -0800

    Started this 8.12 - Eight Queens ridiculousness

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.12/8.12-eight-queens.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.12/8.12-eight-queens.js
@@ -5,3 +5,9 @@
 
 Write an algorithm to print all ways of arranging eight queens on a 8x8 chess board so that none of them share the same row, column, or diagonal. In this case, "diagonal" means all diagonas, not just the two that bisect the board.
 */
+
+var changeChar = function(string, position, char) {
+  var answer = string.split('');
+  answer[position] = char;
+  return answer.join('');
+}
