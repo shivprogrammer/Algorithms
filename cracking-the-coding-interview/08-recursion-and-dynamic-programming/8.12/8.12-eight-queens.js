45c040f1ff5c04ecdc66190f252276a9d07c8903
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-05 18:05:09 -0800

    added bitwiseOp function to 8.12 - Eight Queens

--- cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.12/8.12-eight-queens.js
+++ cracking-the-coding-interview/08-recursion-and-dynamic-programming/8.12/8.12-eight-queens.js
@@ -11,3 +11,19 @@ var changeChar = function(string, position, char) {
   answer[position] = char;
   return answer.join('');
 }
+
+var bitwiseOp = function(binaryString, n, callback) {
+  var base10 = parseInt(binaryString, 2);
+  base10 = callback(base10);
+  var base2 = base10.toString(2);
+  if (base2.length <= n) {
+    var front = '';
+    for (var i = base2.length; i < n; i++) {
+      front += '0';
+    }
+    return front + base2;
+  }
+  else {
+    return base2.slice(base2.length - n);
+  }
+}
