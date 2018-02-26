a9cd841351e7493486a88905ac29cf60d62375c1
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-26 02:54:34 -0800

    added Employee class | 7.2 - Call Center

--- cracking-the-coding-interview/07-object-oriented-design/7.2/7.2-Call_Center.java
+++ cracking-the-coding-interview/07-object-oriented-design/7.2/7.2-Call_Center.java
@@ -77,3 +77,28 @@ public class Call {
    public void incrementRank() { ... }
    public void disconnect() { ... }
 }
+
+// Employee is a super class for the Director, Manager, and Respondent classes. It is implemented as an abstract class since there should be reason to instantiate an Employee directly.
+abstract class Employee {
+  private Call currentCall = null;
+  protected Rank rank;
+
+  public Employee(CallHandler handler) { ... }
+
+  // Start the conversation
+  public void receiveCall(Call call) { ... }
+
+  // the issue is resolved, finish the call
+  public void callCompleted() { ... }
+
+  // The issue has not been resolved. Escalate the call, and assign a new call to the employee
+  public void escalateAndReassign() { ... }
+
+  // Assign a new call to an employee, if the employee is free
+  public boolean assignNewCall() { ... }
+
+  // Returns whether or not the employee is free
+  public boolean isFree() { return currentCall == null; }
+
+  public Rank getRank() { return rank; }
+}
