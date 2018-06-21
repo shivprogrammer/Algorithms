248cfae307b115a995a74e3f9babbd1371eee7fe
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-20 20:20:57 -0700

    attacking the big dogs now, this is snail 4kyu

--- codewars/working-on/snail.js
+++ codewars/working-on/snail.js
@@ -22,59 +22,78 @@ snail(array) #=> [1,2,3,4,5,6,7,8,9]
 */
 
 function snail(matrix) {
-  var output = [];
-  var completedMatrix = [];
-  var temp = [];
-  var row = matrix.length;
-  var col = matrix[0].length;
 
-  for (let a = 0; a < matrix.length; a++) {
-    for (let b = 0; b < matrix[0].length; b++) {
-      temp.push('');
-    }
-    completedMatrix.push(temp);
-    temp = [];
-  }
-
-  for (let x = 0; x < row; x++) {
-    for (let y = 0; y < col; y++) {
-      completedMatrix[x][y] = true;
-      output.push(matrix[x][y]);
-    }
-    row--;
-  }
-
-  console.log(completedMatrix);
-  console.log(output);
 }
 
-var matrix1 = [
+var matrix2A = [
+  [7, 4],
+  [3, 2]
+];
+
+var matrix3A = [
   [1,2,3],
   [4,5,6],
   [7,8,9]
 ];
 
-var matrix2 = [
+var matrix3B = [
   [1,2,3],
   [8,9,4],
   [7,6,5]
 ];
 
-describe('Snail | Code Wars | 4kyu', function() {
-  describe('Input: matrix1', function() {
+// describe('Snail | Code Wars | 4kyu', function() {
+  describe('Input: matrix2A', function() {
+    it('should return [1,2,3,4,5,6,7,8,9]', done => {
+      expect(snail(matrix2)).to.deep.equal([1,2,3,4,5,6,7,8,9]);
+      done();
+    })
+  })
+
+  describe('Input: matrix3A', function() {
     it('should return [1,2,3,6,9,8,7,4,5]', done => {
       expect(snail(matrix1)).to.deep.equal([1,2,3,6,9,8,7,4,5]);
       done();
     })
   })
 
-  describe('Input: matrix2', function() {
+  describe('Input: matrix3B', function() {
     it('should return [1,2,3,4,5,6,7,8,9]', done => {
       expect(snail(matrix2)).to.deep.equal([1,2,3,4,5,6,7,8,9]);
       done();
     })
   })
 })
+
+////////// PREVIOUS SOLUTION
+// function snail(matrix) {
+//   var output = [];
+//   var completedMatrix = [];
+//   var temp = [];
+//   var row = matrix.length;
+//   var col = matrix[0].length;
+//
+//   for (let a = 0; a < matrix.length; a++) {
+//     for (let b = 0; b < matrix[0].length; b++) {
+//       temp.push('');
+//     }
+//     completedMatrix.push(temp);
+//     temp = [];
+//   }
+//
+//   for (let x = 0; x < row; x++) {
+//     for (let y = 0; y < col; y++) {
+//       completedMatrix[x][y] = true;
+//       output.push(matrix[x][y]);
+//     }
+//     row--;
+//   }
+//
+//   console.log(completedMatrix);
+//   console.log(output);
+// }
+
+///// PREVIOUSLY COMMENTED OUT INFORMATION
 
 // var width = array.length;
 // // console.log(width);
